db6085a12c4903ba437e3386f1ed666e8731b6d5
Vasiliy Migel <vasiliy.migel@spryker.com>
2018-02-19 06:28:00 +0100

    CORE-2414 fix Travis after code sniffer

--- Bundles/FileManager/src/SprykerShop/Yves/FileManager/FileManagerFactory.php
+++ Bundles/FileManager/src/SprykerShop/Yves/FileManager/FileManagerFactory.php
@@ -1,21 +1,21 @@
 <?php
 
+/**
+ * Copyright © 2016-present Spryker Systems GmbH. All rights reserved.
+ * Use of this software requires acceptance of the Evaluation License Agreement. See LICENSE file.
+ */
+
 namespace SprykerShop\Yves\FileManager;
 
-
-use Spryker\Service\FileManager\FileManagerService;
 use Spryker\Yves\Kernel\AbstractFactory;
 
 class FileManagerFactory extends AbstractFactory
 {
-
     /**
-     * @return FileManagerService
-     * @throws \Spryker\Yves\Kernel\Exception\Container\ContainerKeyNotFoundException
+     * @return \Spryker\Service\FileManager\FileManagerService
      */
     public function getFileManagerService()
     {
         return $this->getProvidedDependency(FileManagerDependencyProvider::FILE_MANAGER_SERVICE);
     }
-
 }
