45fd082c816afa7c8335985e283d399a87353f51
Ahmed Sabaa <ahmed.sabaa@spryker.com>
2018-02-26 15:09:18 +0000

    CORE-2297 fixed composer.json dependencies

--- Bundles/ShopLayout/src/SprykerShop/Yves/ShopLayout/ShopLayoutDependencyProvider.php
+++ Bundles/ShopLayout/src/SprykerShop/Yves/ShopLayout/ShopLayoutDependencyProvider.php
@@ -10,7 +10,6 @@ namespace SprykerShop\Yves\ShopLayout;
 use Spryker\Shared\Kernel\Store;
 use Spryker\Yves\Kernel\AbstractBundleDependencyProvider;
 use Spryker\Yves\Kernel\Container;
-use SprykerShop\Yves\ShopLayoutExtension\Dependency\Plugin\LanguageSwitcherWidget\LanguageSwitcherWidgetPluginInterface;
 
 class ShopLayoutDependencyProvider extends AbstractBundleDependencyProvider
 {
@@ -35,18 +34,10 @@ class ShopLayoutDependencyProvider extends AbstractBundleDependencyProvider
      */
     protected function addStore(Container $container)
     {
-        $container[self::STORE] = function () {
+        $container[static::STORE] = function () {
             return Store::getInstance();
         };
 
         return $container;
-    }
-
-    /**
-     * @return void
-     */
-    private function requireExtensions()
-    {
-        class_exists(LanguageSwitcherWidgetPluginInterface::class);
     }
 }
