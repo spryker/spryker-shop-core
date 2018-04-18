5eb3755eaec0d6f5ea6d39c9a9446df3a03361a1
Roman Shopin <shopin.roman@gmail.com>
2018-04-18 11:31:27 +0300

    CORE-4609: Min choice quantity calculation fix

--- Bundles/ProductMeasurementUnitWidget/src/SprykerShop/Yves/ProductMeasurementUnitWidget/Theme/default/components/molecules/measurement-quantity-selector/measurement-quantity-selector.ts
+++ Bundles/ProductMeasurementUnitWidget/src/SprykerShop/Yves/ProductMeasurementUnitWidget/Theme/default/components/molecules/measurement-quantity-selector/measurement-quantity-selector.ts
@@ -163,7 +163,7 @@ export default class MeasurementQuantitySelector extends Component {
             return this.getMinQuantity();
         }
 
-        if ((qtyInBaseUnits - this.getMinQuantity()) % this.getQuantityInterval() !== 0 || qtyInBaseUnits > this.getMaxQuantity()) {
+        if ((qtyInBaseUnits - this.getMinQuantity()) % this.getQuantityInterval() !== 0 || (this.getMaxQuantity() > 0 && qtyInBaseUnits > this.getMaxQuantity())) {
             return this.getMinChoice((qtyInBaseUnits - 1) / this.currentSalesUnit.conversion)
         }
 
@@ -220,7 +220,7 @@ export default class MeasurementQuantitySelector extends Component {
     }
 
     getMaxQuantity() {
-        if (this.productQuantityStorage.hasOwnProperty('quantity_max')) {
+        if (this.productQuantityStorage.hasOwnProperty('quantity_max') && this.productQuantityStorage.quantity_max !== null) {
             return this.productQuantityStorage.quantity_max;
         }
 
