8a728f1274a6c652d9126df3405c027f3e50dc6e
Theodoros Liokos <theodoros.liokos@spryker.com>
2018-04-23 18:02:49 +0000

    CORE-2663: Moved Invitations from Customer to Company page

--- Bundles/CompanyUserInvitationPage/src/SprykerShop/Yves/CompanyUserInvitationPage/Plugin/Provider/CompanyUserInvitationPageControllerProvider.php
+++ Bundles/CompanyUserInvitationPage/src/SprykerShop/Yves/CompanyUserInvitationPage/Plugin/Provider/CompanyUserInvitationPageControllerProvider.php
@@ -13,19 +13,19 @@ use SprykerShop\Yves\ShopApplication\Plugin\Provider\AbstractYvesControllerProvi
 
 class CompanyUserInvitationPageControllerProvider extends AbstractYvesControllerProvider
 {
-    public const ROUTE_OVERVIEW = 'company-user-invitation';
-    public const ROUTE_GET_IMPORT_ERRORS = 'company-user-invitation/get-import-errors';
+    public const ROUTE_OVERVIEW = 'company/user-invitation';
+    public const ROUTE_GET_IMPORT_ERRORS = 'company/user-invitation/get-import-errors';
 
-    public const ROUTE_INVITATION_SEND = 'company-user-invitation/send';
-    public const ROUTE_INVITATION_SEND_ALL = 'company-user-invitation/send-all';
+    public const ROUTE_INVITATION_SEND = 'company/user-invitation/send';
+    public const ROUTE_INVITATION_SEND_ALL = 'company/user-invitation/send-all';
 
-    public const ROUTE_INVITATION_RESEND = 'company-user-invitation/resend';
-    public const ROUTE_INVITATION_RESEND_CONFIRM = 'company-user-invitation/resend/confirm';
+    public const ROUTE_INVITATION_RESEND = 'company/user-invitation/resend';
+    public const ROUTE_INVITATION_RESEND_CONFIRM = 'company/user-invitation/resend/confirm';
 
     public const ROUTE_INVITATION_ACCEPT = CompanyUserInvitationPageConstants::ROUTE_INVITATION_ACCEPT;
 
-    public const ROUTE_INVITATION_DELETE = 'company-user-invitation/delete';
-    public const ROUTE_INVITATION_DELETE_CONFIRM = 'company-user-invitation/delete/confirm';
+    public const ROUTE_INVITATION_DELETE = 'company/user-invitation/delete';
+    public const ROUTE_INVITATION_DELETE_CONFIRM = 'company/user-invitation/delete/confirm';
 
     /**
      * @param \Silex\Application $app
@@ -37,36 +37,36 @@ class CompanyUserInvitationPageControllerProvider extends AbstractYvesController
         $allowedLocalesPattern = $this->getAllowedLocalesPattern();
 
         $this->createController('/{companyUserInvitation}', static::ROUTE_OVERVIEW, 'CompanyUserInvitationPage', 'Import')
-            ->assert('companyUserInvitation', $allowedLocalesPattern . 'company-user-invitation|company-user-invitation')
-            ->value('companyUserInvitation', 'company-user-invitation');
+            ->assert('companyUserInvitation', $allowedLocalesPattern . 'company/user-invitation|company/user-invitation')
+            ->value('companyUserInvitation', 'company/user-invitation');
 
         $this->createController('/{companyUserInvitation}/get-import-errors', static::ROUTE_GET_IMPORT_ERRORS, 'CompanyUserInvitationPage', 'Import', 'getErrors')
-            ->assert('companyUserInvitation', $allowedLocalesPattern . 'company-user-invitation|company-user-invitation')
-            ->value('companyUserInvitation', 'company-user-invitation');
+            ->assert('companyUserInvitation', $allowedLocalesPattern . 'company/user-invitation|company/user-invitation')
+            ->value('companyUserInvitation', 'company/user-invitation');
 
         $this->createController('/{companyUserInvitation}/send', static::ROUTE_INVITATION_SEND, 'CompanyUserInvitationPage', 'Send', 'sendCompanyUserInvitation')
-            ->assert('companyUserInvitation', $allowedLocalesPattern . 'company-user-invitation|company-user-invitation')
-            ->value('companyUserInvitation', 'company-user-invitation');
+            ->assert('companyUserInvitation', $allowedLocalesPattern . 'company/user-invitation|company/user-invitation')
+            ->value('companyUserInvitation', 'company/user-invitation');
 
         $this->createController('/{companyUserInvitation}/send-all', static::ROUTE_INVITATION_SEND_ALL, 'CompanyUserInvitationPage', 'Send', 'sendCompanyUserInvitations')
-            ->assert('companyUserInvitation', $allowedLocalesPattern . 'company-user-invitation|company-user-invitation')
-            ->value('companyUserInvitation', 'company-user-invitation');
+            ->assert('companyUserInvitation', $allowedLocalesPattern . 'company/user-invitation|company/user-invitation')
+            ->value('companyUserInvitation', 'company/user-invitation');
 
         $this->createController('/{companyUserInvitation}/resend', static::ROUTE_INVITATION_RESEND, 'CompanyUserInvitationPage', 'Resend')
-            ->assert('companyUserInvitation', $allowedLocalesPattern . 'company-user-invitation|company-user-invitation')
-            ->value('companyUserInvitation', 'company-user-invitation');
+            ->assert('companyUserInvitation', $allowedLocalesPattern . 'company/user-invitation|company/user-invitation')
+            ->value('companyUserInvitation', 'company/user-invitation');
 
         $this->createController('/{companyUserInvitation}/resend/confirm', static::ROUTE_INVITATION_RESEND_CONFIRM, 'CompanyUserInvitationPage', 'Resend', 'confirm')
-            ->assert('companyUserInvitation', $allowedLocalesPattern . 'company-user-invitation|company-user-invitation')
-            ->value('companyUserInvitation', 'company-user-invitation');
+            ->assert('companyUserInvitation', $allowedLocalesPattern . 'company/user-invitation|company/user-invitation')
+            ->value('companyUserInvitation', 'company/user-invitation');
 
         $this->createController('/{companyUserInvitation}/delete', static::ROUTE_INVITATION_DELETE, 'CompanyUserInvitationPage', 'Delete')
-            ->assert('companyUserInvitation', $allowedLocalesPattern . 'company-user-invitation|company-user-invitation')
-            ->value('companyUserInvitation', 'company-user-invitation');
+            ->assert('companyUserInvitation', $allowedLocalesPattern . 'company/user-invitation|company/user-invitation')
+            ->value('companyUserInvitation', 'company/user-invitation');
 
         $this->createController('/{companyUserInvitation}/delete/confirm', static::ROUTE_INVITATION_DELETE_CONFIRM, 'CompanyUserInvitationPage', 'Delete', 'confirm')
-            ->assert('companyUserInvitation', $allowedLocalesPattern . 'company-user-invitation|company-user-invitation')
-            ->value('companyUserInvitation', 'company-user-invitation');
+            ->assert('companyUserInvitation', $allowedLocalesPattern . 'company/user-invitation|company/user-invitation')
+            ->value('companyUserInvitation', 'company/user-invitation');
 
         $this->createController('/{invitation}/accept', static::ROUTE_INVITATION_ACCEPT, 'CompanyUserInvitationPage', 'Accept')
             ->assert('invitation', $allowedLocalesPattern . 'invitation|invitation')
