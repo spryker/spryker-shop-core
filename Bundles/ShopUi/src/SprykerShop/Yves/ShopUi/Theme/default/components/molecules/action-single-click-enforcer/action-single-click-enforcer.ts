e6f7a2f70ab150020bf222a20035f25d0cf21f56
maslov <dev.maslov.a@gmail.com>
2018-05-16 21:09:11 +0300

    PS-542: fixed issues according with code review

--- Bundles/ShopUi/src/SprykerShop/Yves/ShopUi/Theme/default/components/molecules/action-single-click-enforcer/action-single-click-enforcer.ts
+++ Bundles/ShopUi/src/SprykerShop/Yves/ShopUi/Theme/default/components/molecules/action-single-click-enforcer/action-single-click-enforcer.ts
@@ -1,36 +1,45 @@
 import Component from '../../../models/component';
 
 export default class ActionSingleClickEnforcer extends Component {
-    readonly triggers: HTMLElement[]
+    readonly targets: HTMLElement[]
 
     constructor() {
         super();
-        this.triggers = <HTMLElement[]>Array.from(document.querySelectorAll(this.targetSelector));
+        this.targets = this.hasSelector ? <HTMLElement[]>Array.from(document.querySelectorAll(this.targetSelector)) : [];
     }
 
     readyCallback(): void {
-        this.mapEvents();
+        if(this.hasSelector) {
+            this.mapEvents();
+        }
     }
 
     mapEvents(): void {
-        this.triggers.forEach((element: HTMLElement) => {
-            element.addEventListener('click', (event: Event) => this.onTriggerClick(event));
+        this.targets.forEach((element: HTMLElement) => {
+            element.addEventListener('click', (event: Event) => this.onTargetsClick(event));
         });
     }
 
-    onTriggerClick(event: Event): void {
-        let htmlElement = <HTMLElement> event.target;
+    onTargetsClick(event: Event): void {
+        const targetElement = <HTMLElement> event.currentTarget;
+        const hasAttribute: boolean= targetElement.hasAttribute('disabled');
 
-        const hasAttribute: boolean= htmlElement.hasAttribute('disabled');
         if(hasAttribute) {
             event.preventDefault();
             return;
         }
-        htmlElement.setAttribute('disabled', 'disabled');
+        targetElement.setAttribute('disabled', 'disabled');
     }
 
     get targetSelector(): string {
         return this.getAttribute('target-selector');
+    }
+
+    get hasSelector(): boolean {
+        if(this.targetSelector) {
+            return true;
+        }
+        return false;
     }
 
 }
