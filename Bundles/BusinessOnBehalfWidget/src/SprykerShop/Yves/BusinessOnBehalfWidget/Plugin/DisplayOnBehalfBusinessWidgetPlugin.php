cebb2192672a37fd8ac4d90e4f2ad0569bd64e07
Sergey Sikachev <sergey.sikachev@spryker.com>
2018-05-18 17:21:34 +0300

    PS-809. Plugins partially created.

---
+++ Bundles/BusinessOnBehalfWidget/src/SprykerShop/Yves/BusinessOnBehalfWidget/Plugin/DisplayOnBehalfBusinessWidgetPlugin.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * This file is part of the Spryker Demoshop.
+ * For full license information, please view the LICENSE file that was distributed with this source code.
+ */
+
+namespace SprykerShop\Yves\BusinessOnBehalfWidget\Plugin;
+
+use Spryker\Yves\Kernel\AbstractPlugin;
+
+class DisplayOnBehalfBusinessWidgetPlugin extends AbstractPlugin implements DisplayOnBehalfBusinessWidgetPluginInterface
+{
+
+}
---
+++ Bundles/BusinessOnBehalfWidget/src/SprykerShop/Yves/BusinessOnBehalfWidget/Plugin/DisplayOnBehalfBusinessWidgetPluginInterface.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace SprykerShop\Yves\BusinessOnBehalfWidget\Plugin;
+
+interface DisplayOnBehalfBusinessWidgetPluginInterface
+{
+
+}
