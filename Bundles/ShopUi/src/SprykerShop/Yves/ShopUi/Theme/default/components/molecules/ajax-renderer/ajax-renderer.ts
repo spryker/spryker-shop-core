efb5a445a78daf693d5f610a282f18ca064f46f5
ilmente <alessandro.bellini@spryker.com>
2018-06-04 15:04:48 +0200

    CORE-4328: migrate wishlist widget to atomic frontend

---
+++ Bundles/ShopUi/src/SprykerShop/Yves/ShopUi/Theme/default/components/molecules/ajax-renderer/ajax-renderer.ts
@@ -0,0 +1,39 @@
+import Component from '../../../models/component';
+import AjaxProvider from '../ajax-provider/ajax-provider';
+
+export default class AjaxRenderer extends Component {
+    protected provider: AjaxProvider
+    protected target: HTMLElement
+
+    readyCallback() {
+        this.provider = <AjaxProvider>document.querySelector(this.providerSelector);
+        this.target = this.targetSelector ? <HTMLElement>document.querySelector(this.targetSelector) : this;
+        this.mapEvents();
+    }
+
+    protected mapEvents() {
+        this.provider.addEventListener('fetched', (event: Event) => this.onFetched(event));
+    }
+
+    protected onFetched(event: Event): void {
+        const response = this.provider.xhr.response;
+
+        console.log();
+
+        if (!!response.trim() || this.renderIfResponseIsEmpty) {
+            this.target.innerHTML = response;
+        }
+    }
+
+    get providerSelector(): string {
+        return this.getAttribute('provider-selector');
+    }
+
+    get targetSelector(): string {
+        return this.getAttribute('target-selector') || '';
+    }
+
+    get renderIfResponseIsEmpty(): boolean {
+        return this.hasAttribute('render-if-response-is-empty');
+    }
+}
