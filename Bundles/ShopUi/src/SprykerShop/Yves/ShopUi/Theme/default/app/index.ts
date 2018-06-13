a1e7800a3f4683aefa8db9d7fa3531d22b8da22d
ilmente <alessandro.bellini@spryker.com>
2018-06-13 12:00:48 +0200

    te-7: add es6-polyfill to main layout

--- Bundles/ShopUi/src/SprykerShop/Yves/ShopUi/Theme/default/app/index.ts
+++ Bundles/ShopUi/src/SprykerShop/Yves/ShopUi/Theme/default/app/index.ts
@@ -1,5 +1,5 @@
 import Candidate from './candidate';
-import { LogLevel, log, error, config as setLogConfig } from './logger';
+import { LogLevel, log, info, error, config as setLogConfig } from './logger';
 import { candidates } from './registry';
 import { get as config, set as setConfig, defaultConfig, Config } from './config';
 
@@ -18,21 +18,22 @@ export async function mount(): Promise<void[]> {
 }
 
 async function onDOMContentLoaded(): Promise<void> {
+    log('DOM loaded');
+
     try {
         await mount();
         fireReadyEvent();
         log('application ready');
     } catch (err) {
         fireErrorEvent(err);
-        error('application failed\n', err);
+        error('application error\n', err);
     }
 }
 
 export async function bootstrap(appConfig: Config = defaultConfig): Promise<void> {
     setConfig(appConfig);
     setLogConfig(config().log.level, config().log.prefix);
-
-    log('mode:', config().isProduction ? 'PRODUCTION,' : 'DEVELOPMENT,', 'log-level:', LogLevel[config().log.level]);
+    info('mode:', config().isProduction ? 'PRODUCTION,' : 'DEVELOPMENT,', 'log-level:', LogLevel[config().log.level]);
 
     document.addEventListener('DOMContentLoaded', () => onDOMContentLoaded());
 }
