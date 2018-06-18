75351ce884dd7002058f4fcfca0f3a0f0016a2e7
Ruslan Dovhospynyi <dovhospynyi@MacBook-Pro-15-Dovgospiniy.local>
2018-06-18 10:19:36 +0300

    PS-1901 add email validation for newsletter subscription widget

--- Bundles/NewsletterWidget/src/SprykerShop/Yves/NewsletterWidget/Form/NewsletterSubscriptionForm.php
+++ Bundles/NewsletterWidget/src/SprykerShop/Yves/NewsletterWidget/Form/NewsletterSubscriptionForm.php
@@ -12,6 +12,7 @@ use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Validator\Constraints\NotBlank;
+use Symfony\Component\Validator\Constraints\Email;
 
 class NewsletterSubscriptionForm extends AbstractType
 {
@@ -66,6 +67,7 @@ class NewsletterSubscriptionForm extends AbstractType
             'required' => false,
             'constraints' => [
                 new NotBlank(),
+                new Email(),
             ],
         ]);
 
