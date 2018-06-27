898dd16fdf91566888f16c853b0fb10dac5e0f82
galitsky <eugen.galitsky@spryker.com>
2018-06-27 16:50:13 +0300

    PS-2422 volume price markup and styles by atomic

---
+++ Bundles/VolumePriceProductWidget/src/SprykerShop/Yves/VolumePriceProductWidget/Theme/default/components/molecules/volume-price/volume-price.ts
@@ -0,0 +1,7 @@
+import Component from 'ShopUi/models/component';
+
+export default class VolumePrice extends Component {
+    protected readyCallback(): void {
+        console.log('init')
+    }
+}
