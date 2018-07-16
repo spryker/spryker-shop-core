18b3879d75862bcc293e53f0c6262146e115c1ea
mscherer <mark.scherer@spryker.com>
2018-07-16 16:30:17 +0200

    Add note about constants interfaces.

--- Bundles/QuickOrderPage/src/SprykerShop/Shared/QuickOrderPage/QuickOrderPageConstants.php
+++ Bundles/QuickOrderPage/src/SprykerShop/Shared/QuickOrderPage/QuickOrderPageConstants.php
@@ -7,9 +7,18 @@
 
 namespace SprykerShop\Shared\QuickOrderPage;
 
+/**
+ * Declares global environment configuration keys. Do not use it for other class constants.
+ */
 interface QuickOrderPageConstants
 {
+    /**
+     * Deprecated: Move to Config instead.
+     */
     public const ALLOWED_SEPARATORS = 'ALLOWED_SEPARATORS';
 
+    /**
+     * Deprecated: Move to Config instead.
+     */
     public const PRODUCT_ROWS_NUMBER = 'PRODUCT_ROWS_NUMBER';
 }
