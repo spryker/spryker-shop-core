9b772cb9443cb53aa411fd642e95f33e184ce526
Oleksandr Myrnyi <oleksandr.myrnyi@spryker.com>
2018-07-26 09:30:55 +0300

    update parameter type

--- Bundles/CompanyPage/src/SprykerShop/Yves/CompanyPage/Model/CompanyBusinessUnit/CompanyBusinessUnitTreeBuilder.php
+++ Bundles/CompanyPage/src/SprykerShop/Yves/CompanyPage/Model/CompanyBusinessUnit/CompanyBusinessUnitTreeBuilder.php
@@ -74,7 +74,7 @@ class CompanyBusinessUnitTreeBuilder implements CompanyBusinessUnitTreeBuilderIn
      *
      * @return array
      */
-    protected function buildTree(ArrayObject $companyBusinessUnits, $idParentCompanyBusinessUnit = null, int $indent = 0): array
+    protected function buildTree(ArrayObject $companyBusinessUnits, ?int $idParentCompanyBusinessUnit = null, int $indent = 0): array
     {
         $tree = [];
         foreach ($companyBusinessUnits as $companyBusinessUnit) {
