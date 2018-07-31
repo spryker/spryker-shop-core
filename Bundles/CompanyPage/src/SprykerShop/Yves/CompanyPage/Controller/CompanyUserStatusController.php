21d9915d2bdb280fb5c9ff47488d6bd820d57e7d
Vitaliy Kirichenko <vitaly.kirichenko@spryker.com>
2018-07-31 15:37:46 +0300

    PS-2890: Created CompanyUserStatusController with methods to enable / disable company user

---
+++ Bundles/CompanyPage/src/SprykerShop/Yves/CompanyPage/Controller/CompanyUserStatusController.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace SprykerShop\Yves\CompanyPage\Controller;
+
+use Generated\Shared\Transfer\CompanyUserTransfer;
+use Spryker\Yves\Kernel\Controller\AbstractController;
+use SprykerShop\Yves\CompanyPage\Plugin\Provider\CompanyPageControllerProvider;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Request;
+
+/**
+ * @method \SprykerShop\Yves\CompanyPage\CompanyPageFactory getFactory()
+ */
+class CompanyUserStatusController extends AbstractController
+{
+    /**
+     * @param \Symfony\Component\HttpFoundation\Request $request
+     *
+     * @return \Symfony\Component\HttpFoundation\RedirectResponse
+     */
+    public function enableCompanyUserAction(Request $request): RedirectResponse
+    {
+        $idCompanyUser = $request->query->getInt('id-company-user');
+        $companyUserTransfer = (new CompanyUserTransfer())
+            ->setIdCompanyUser($idCompanyUser);
+
+        $this->getFactory()
+            ->getCompanyUserClient()
+            ->enableCompanyUser($companyUserTransfer);
+
+        return $this->redirectResponseInternal(CompanyPageControllerProvider::ROUTE_COMPANY_USER);
+    }
+
+    /**
+     * @param \Symfony\Component\HttpFoundation\Request $request
+     *
+     * @return \Symfony\Component\HttpFoundation\RedirectResponse
+     */
+    public function disableCompanyUserAction(Request $request): RedirectResponse
+    {
+        $idCompanyUser = $request->query->getInt('id-company-user');
+        $companyUserTransfer = (new CompanyUserTransfer())
+            ->setIdCompanyUser($idCompanyUser);
+
+        $this->getFactory()
+            ->getCompanyUserClient()
+            ->disableCompanyUser($companyUserTransfer);
+
+        return $this->redirectResponseInternal(CompanyPageControllerProvider::ROUTE_COMPANY_USER);
+    }
+}
