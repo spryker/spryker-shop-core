def8254d7eb0e5e5538a21cd70cc856ac2a79a2f
Vitaliy Kirichenko <vitaly.kirichenko@spryker.com>
2018-08-02 17:27:39 +0300

    PS-3623: Updated deletion confirmation page accordingly to Andrew's requirements

--- Bundles/CompanyPage/src/SprykerShop/Yves/CompanyPage/Controller/UserDeleteController.php
+++ Bundles/CompanyPage/src/SprykerShop/Yves/CompanyPage/Controller/UserDeleteController.php
@@ -7,6 +7,7 @@
 
 namespace SprykerShop\Yves\CompanyPage\Controller;
 
+use Generated\Shared\Transfer\CompanyUserTransfer;
 use Spryker\Yves\Kernel\Controller\AbstractController;
 use Spryker\Yves\Kernel\View\View;
 use Symfony\Component\HttpFoundation\Request;
@@ -18,6 +19,8 @@ class UserDeleteController extends AbstractController
 {
     protected const PARAM_REQUEST_ID_COMPANY_USER = 'id';
     protected const PARAM_RESPONSE_ID_COMPANY_USER = 'idCompanyUser';
+    protected const PARAM_RESPONSE_FIRST_NAME_COMPANY_USER = 'firstName';
+    protected const PARAM_RESPONSE_LAST_NAME_COMPANY_USER = 'lastName';
 
     /**
      * @param \Symfony\Component\HttpFoundation\Request $request
@@ -28,8 +31,21 @@ class UserDeleteController extends AbstractController
     {
         $idCompanyUser = $request->query->getInt(static::PARAM_REQUEST_ID_COMPANY_USER);
 
+        $companyUserTransfer = (new CompanyUserTransfer())
+            ->setIdCompanyUser($idCompanyUser);
+
+        $companyUserTransfer->requireIdCompanyUser();
+        $companyUserTransfer = $this->getFactory()
+            ->getCompanyUserClient()
+            ->getCompanyUserById($companyUserTransfer);
+
+        $companyUserTransfer->requireCustomer();
+        $customerTransfer = $companyUserTransfer->getCustomer();
+
         return $this->view([
             static::PARAM_RESPONSE_ID_COMPANY_USER => $idCompanyUser,
+            static::PARAM_RESPONSE_FIRST_NAME_COMPANY_USER => $customerTransfer->getFirstName(),
+            static::PARAM_RESPONSE_LAST_NAME_COMPANY_USER => $customerTransfer->getLastName(),
         ], [], '@CompanyPage/views/user-delete/user-delete.twig');
     }
 }
