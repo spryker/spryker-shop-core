3dec502f64beb6f372cc964e933c7d85d07c7e08
ruslan.dovhospynyi <ruslan.dovhospynyi@spryker.com>
2018-08-06 18:40:53 +0300

    PS-2191: move plugin to another folder

--- Bundles/CheckoutPage/src/SprykerShop/Client/CheckoutPage/Plugin/PlaceOrderWithAmountUpToPermissionPlugin.php
+++ Bundles/CheckoutPage/src/SprykerShop/Client/CheckoutPage/Plugin/PlaceOrderWithAmountUpToPermissionPlugin.php
@@ -12,7 +12,7 @@ use Spryker\Shared\PermissionExtension\Dependency\Plugin\ExecutablePermissionPlu
 /**
  * For Client PermissionDependencyProvider::getPermissionPlugins() registration
  *
- * @deprecated Use \Spryker\Client\CheckoutPermissionConnector\Plugin\PlaceOrderWithAmountUpToPermissionPlugin instead
+ * @deprecated Use \Spryker\Client\CheckoutPermissionConnector\Plugin\PermissionExtension\PlaceOrderWithAmountUpToPermissionPlugin instead
  */
 class PlaceOrderWithAmountUpToPermissionPlugin implements ExecutablePermissionPluginInterface
 {
