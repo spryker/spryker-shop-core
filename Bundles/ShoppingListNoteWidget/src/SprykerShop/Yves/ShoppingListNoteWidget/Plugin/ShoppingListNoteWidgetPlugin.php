02b1561dac0dea0b92e3cf1d8b2bffb45b5ef617
ruslan.dovhospynyi <ruslan.dovhospynyi@spryker.com>
2018-08-11 16:21:55 +0300

    PS-3098 Create widget SprykerShop\ShoppingListNoteWidget with template to display notes

---
+++ Bundles/ShoppingListNoteWidget/src/SprykerShop/Yves/ShoppingListNoteWidget/Plugin/ShoppingListNoteWidgetPlugin.php
@@ -0,0 +1,67 @@
+<?php
+
+/**
+ * Copyright © 2016-present Spryker Systems GmbH. All rights reserved.
+ * Use of this software requires acceptance of the Evaluation License Agreement. See LICENSE file.
+ */
+
+namespace SprykerShop\Yves\ShoppingListNoteWidget\Plugin;
+
+use Generated\Shared\Transfer\ProductViewTransfer;
+use Generated\Shared\Transfer\ShoppingListItemNoteTransfer;
+use Generated\Shared\Transfer\ShoppingListTransfer;
+use Spryker\Yves\Kernel\Widget\AbstractWidgetPlugin;
+use SprykerShop\Yves\ShoppingListPage\Plugin\ShoppingListItemNoteWidgetPluginInterface;
+
+class ShoppingListNoteWidgetPlugin extends AbstractWidgetPlugin implements ShoppingListItemNoteWidgetPluginInterface
+{
+    /**
+     * @var \Generated\Shared\Transfer\ProductViewTransfer
+     */
+    protected $productViewTransfer;
+
+    /**
+     * @param \Generated\Shared\Transfer\ProductViewTransfer $productViewTransfer
+     * @param \Generated\Shared\Transfer\ShoppingListTransfer $shoppingListTransfer
+     *
+     * @return void
+     */
+    public function initialize(ProductViewTransfer $productViewTransfer, ShoppingListTransfer $shoppingListTransfer): void
+    {
+        $this->productViewTransfer = $productViewTransfer;
+        $this->addParameter('note', $this->getShoppingListItemNote($productViewTransfer, $shoppingListTransfer));
+    }
+
+    /**
+     * @return string
+     */
+    public static function getTemplate(): string
+    {
+        return '@ShoppingListNoteWidget/views/shopping-list-item-note/shopping-list-item-note.twig';
+    }
+
+    /**
+     * @return string
+     */
+    public static function getName(): string
+    {
+        return static::NAME;
+    }
+
+    /**
+     * @param \Generated\Shared\Transfer\ProductViewTransfer $productViewTransfer
+     * @param \Generated\Shared\Transfer\ShoppingListTransfer $shoppingListTransfer
+     *
+     * @return \Generated\Shared\Transfer\ShoppingListItemNoteTransfer
+     */
+    public function getShoppingListItemNote(ProductViewTransfer $productViewTransfer, ShoppingListTransfer $shoppingListTransfer): ShoppingListItemNoteTransfer
+    {
+        foreach ($shoppingListTransfer->getItems() as $shoppingListItemTransfer) {
+            if ($shoppingListItemTransfer->getSku() === $productViewTransfer->getSku()) {
+                return $shoppingListItemTransfer->getNote();
+            }
+        }
+
+        return new ShoppingListItemNoteTransfer();
+    }
+}
