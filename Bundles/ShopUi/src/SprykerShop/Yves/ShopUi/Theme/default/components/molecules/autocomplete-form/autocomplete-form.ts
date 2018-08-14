1b959f2a964b8e05ef5b96ee89db0d6560fed70c
bezkorovaina <irina.bezkorovayna@spryker.com>
2018-08-14 19:46:08 +0300

    СС-203: Autocomplete dropdown frontend component
    - refactoring;

--- Bundles/ShopUi/src/SprykerShop/Yves/ShopUi/Theme/default/components/molecules/autocomplete-form/autocomplete-form.ts
+++ Bundles/ShopUi/src/SprykerShop/Yves/ShopUi/Theme/default/components/molecules/autocomplete-form/autocomplete-form.ts
@@ -7,7 +7,6 @@ export default class AutocompleteForm extends Component {
     inputElement: HTMLInputElement;
     hiddenInputElement: HTMLInputElement;
     suggestionsContainer: HTMLElement;
-    target: any;
 
     protected readyCallback(): void {
         this.ajaxProvider = <AjaxProvider> this.querySelector(`.${this.jsName}__provider`);
@@ -19,69 +18,79 @@ export default class AutocompleteForm extends Component {
 
     protected mapEvents(): void {
         this.inputElement.addEventListener('input', debounce(() => this.onInput(), this.debounceDelay));
-        this.inputElement.addEventListener('blur', debounce(() => this.hideSugestions(), this.debounceDelay));
+        this.inputElement.addEventListener('blur', debounce(() => this.onHideSugestions(), this.debounceDelay));
         this.inputElement.addEventListener('focus',  () => this.onFocus());
     }
 
-    protected onFocus() {
-        if (this.inputValue.length >= this.minLetters ) {
-            this.showSugestions();
+    protected onFocus(): void {
+        if (this.inputValue.length >= this.minLetters) {
+            this.onShowSugestions();
+            return;
         }
     }
 
-    protected onInput() {
+    protected onInput(): void {
         if (this.inputValue.length >= this.minLetters ) {
-            this.getSuggestions();
-            return false
+            this.loadSuggestions();
+            return;
         }
-        this.hideSugestions();
+        this.onHideSugestions();
     }
 
-    protected showSugestions(): void {
+    protected onShowSugestions(): void {
         this.suggestionsContainer.classList.remove('is-hidden');
-        this.onClick();
+        this.mapItemEvents();
     }
 
-    protected hideSugestions(): void {
+    protected onHideSugestions(): void {
         this.suggestionsContainer.classList.add('is-hidden');
     }
 
-    protected async getSuggestions(): Promise<void> {
-        this.showSugestions()
+    async loadSuggestions(): Promise<void> {
+        this.onShowSugestions();
         let params = {};
         params[this.queryParamName] = this.inputValue;
-
         await this.ajaxProvider.fetch(params);
     }
 
-
-    protected onClick(): void {
+    protected mapItemEvents(): void {
         const self = this;
-        const items = Array.from(this.suggestionsContainer.querySelectorAll(`.${this.jsName}__item`));
-
-        items.forEach((item) => item.addEventListener('click', (e: Event) => self.handlerClick(e)));
+        const items = Array.from(this.suggestionsContainer.querySelectorAll(this.itemSelector));
+        items.forEach((item: HTMLElement) => item.addEventListener('click', (e: Event) => self.onItemClick(e)));
     }
 
-    protected handlerClick(e): void {
-        const dataValue = e.target.getAttribute(this.valueDataAttribute);
-        this.hiddenInputElement.value = dataValue;
-        this.inputElement.value = e.srcElement.textContent;
+    protected onItemClick(e: Event): void {
+        const dataTarget = <HTMLElement>e.target;
+        const textTarget = <HTMLElement>e.srcElement;
+        const data = dataTarget.getAttribute(this.valueDataAttribute);
+        const text = textTarget.textContent;
+
+        this.setInputs(data, text);
+    }
+
+    setInputs(data: string, text: string): void {
+        this.inputElement.value = text;
+        this.hiddenInputElement.value = data;
     }
 
     protected get minLetters(): number {
-        return Number(this.getAttribute('min-letters'))
+        return Number(this.getAttribute('min-letters'));
     }
 
     protected get inputValue(): string {
-        return this.inputElement.value.trim()
+        return this.inputElement.value.trim();
     }
 
     protected get queryParamName(): string {
-        return this.getAttribute('query-param-name')
+        return this.getAttribute('query-param-name');
     }
 
     protected get valueDataAttribute(): string {
-        return this.getAttribute('value-data-attribute')
+        return this.getAttribute('value-data-attribute');
+    }
+
+    protected get itemSelector(): string {
+        return this.getAttribute('item-selector');
     }
 
     get debounceDelay(): number {
