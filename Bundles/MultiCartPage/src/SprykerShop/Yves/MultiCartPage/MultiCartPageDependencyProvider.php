add6aa05f151d1b7ca2ccb39481fcf6974de2087
Vitaliy Kirichenko <vitaly.kirichenko@spryker.com>
2018-08-20 14:16:02 +0300

    PS-3676: Fixed missing dependency for widget plugins

--- Bundles/MultiCartPage/src/SprykerShop/Yves/MultiCartPage/MultiCartPageDependencyProvider.php
+++ Bundles/MultiCartPage/src/SprykerShop/Yves/MultiCartPage/MultiCartPageDependencyProvider.php
@@ -24,6 +24,7 @@ class MultiCartPageDependencyProvider extends AbstractBundleDependencyProvider
     public function provideDependencies(Container $container)
     {
         $container = $this->addMultiCartClient($container);
+        $container = $this->addCartDeleteCompanyUsersListWidgetPlugins($container);
 
         return $container;
     }
