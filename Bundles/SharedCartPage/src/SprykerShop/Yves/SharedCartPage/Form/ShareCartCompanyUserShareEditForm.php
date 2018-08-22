69fc7e246bc16179fbd1e25abd1a225b56d5d3bc
Vitaliy Kirichenko <vitaly.kirichenko@spryker.com>
2018-08-22 15:56:43 +0300

    PS-3675: Updated ShareCartDetailsForm: Renamed it to ShareCartCompanyUserShareEditForm; Added missing fields

--- Bundles/SharedCartPage/src/SprykerShop/Yves/SharedCartPage/Form/ShareCartCompanyUserShareEditForm.php
+++ Bundles/SharedCartPage/src/SprykerShop/Yves/SharedCartPage/Form/ShareCartCompanyUserShareEditForm.php
@@ -1,5 +1,10 @@
 <?php
 
+/**
+ * Copyright © 2016-present Spryker Systems GmbH. All rights reserved.
+ * Use of this software requires acceptance of the Evaluation License Agreement. See LICENSE file.
+ */
+
 namespace SprykerShop\Yves\SharedCartPage\Form;
 
 use Generated\Shared\Transfer\ShareDetailTransfer;
@@ -10,24 +15,12 @@ use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Validator\Constraints\NotBlank;
 
-class ShareCartCompanyUserEditForm extends AbstractType
+class ShareCartCompanyUserShareEditForm extends AbstractType
 {
-    public const FORM_NAME = 'shareCartDetailsForm';
-
     public const FIELD_ID_QUOTE_COMPANY_USER = 'idQuoteCompanyUser';
     public const FIELD_ID_COMPANY_USER = 'idCompanyUser';
+    public const FIELD_CUSTOMER_NAME = 'customerName';
     public const FIELD_QUOTE_PERMISSION_GROUP = 'quotePermissionGroup';
-
-    public const OPTION_CUSTOMERS = 'OPTION_CUSTOMERS';
-    public const OPTION_PERMISSION_GROUPS = 'OPTION_PERMISSION_GROUPS';
-
-    /**
-     * @return string
-     */
-    public function getBlockPrefix()
-    {
-        return self::FORM_NAME;
-    }
 
     /**
      * @param \Symfony\Component\OptionsResolver\OptionsResolver $resolver
@@ -37,10 +30,10 @@ class ShareCartCompanyUserEditForm extends AbstractType
     public function configureOptions(OptionsResolver $resolver)
     {
         $resolver->setRequired([
-            static::FIELD_ID_QUOTE_COMPANY_USER,
             static::FIELD_ID_COMPANY_USER,
             static::FIELD_QUOTE_PERMISSION_GROUP,
-        ])->setDefaults([
+        ]);
+        $resolver->setDefaults([
             'data_class' => ShareDetailTransfer::class,
         ]);
     }
@@ -53,14 +46,16 @@ class ShareCartCompanyUserEditForm extends AbstractType
      */
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
-        $this->addIdQuoteCompanyUserField($builder)
-            ->addIdCompanyUserField($builder, $options);
+        $this->addIdQuoteCompanyUserField($builder);
+        $this->addIdCompanyUserField($builder);
+        $this->addCustomerNameField($builder);
+        $this->addQuotePermissionGroupField($builder, $options);
     }
 
     /**
      * @param \Symfony\Component\Form\FormBuilderInterface $builder
      *
-     * @return \SprykerShop\Yves\SharedCartPage\Form\ShareCartCompanyUserEditForm
+     * @return $this
      */
     protected function addIdQuoteCompanyUserField(FormBuilderInterface $builder): self
     {
@@ -71,23 +66,24 @@ class ShareCartCompanyUserEditForm extends AbstractType
 
     /**
      * @param \Symfony\Component\Form\FormBuilderInterface $builder
-     * @param array $options
      *
      * @return $this
      */
-    protected function addIdCompanyUserField(FormBuilderInterface $builder, array $options)
+    protected function addIdCompanyUserField(FormBuilderInterface $builder): self
     {
-        $builder->add(static::FIELD_ID_COMPANY_USER, ChoiceType::class, [
-            'choices' => array_flip($options[static::OPTION_CUSTOMERS]),
-            'choices_as_values' => true,
-            'expanded' => false,
-            'required' => true,
-            'placeholder' => 'shared_cart.form.select_customer',
-            'constraints' => [
-                new NotBlank(),
-            ],
-            'label' => 'shared_cart.form.customer',
-        ]);
+        $builder->add(static::FIELD_ID_COMPANY_USER, HiddenType::class);
+
+        return $this;
+    }
+
+    /**
+     * @param \Symfony\Component\Form\FormBuilderInterface $builder
+     *
+     * @return $this
+     */
+    protected function addCustomerNameField(FormBuilderInterface $builder): self
+    {
+        $builder->add(static::FIELD_CUSTOMER_NAME, HiddenType::class);
 
         return $this;
     }
@@ -98,10 +94,10 @@ class ShareCartCompanyUserEditForm extends AbstractType
      *
      * @return $this
      */
-    protected function addQuotePermissionGroupIdQuoteIdField(FormBuilderInterface $builder, array $options)
+    protected function addQuotePermissionGroupField(FormBuilderInterface $builder, array $options): self
     {
         $builder->add(static::FIELD_QUOTE_PERMISSION_GROUP, ChoiceType::class, [
-            'choices' => array_flip($options[static::OPTION_PERMISSION_GROUPS]),
+            'choices' => array_flip($options[ShareCartForm::OPTION_PERMISSION_GROUPS]),
             'choices_as_values' => true,
             'expanded' => false,
             'required' => true,
@@ -113,6 +109,4 @@ class ShareCartCompanyUserEditForm extends AbstractType
 
         return $this;
     }
-
-    // TODO: Finish sub-form.
 }
