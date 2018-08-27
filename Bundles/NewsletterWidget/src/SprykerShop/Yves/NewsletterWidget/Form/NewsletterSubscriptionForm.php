f692c25870c600ecd7e7abf7662b7018e8663ddc
ruslan.dovhospynyi <ruslan.dovhospynyi@spryker.com>
2018-08-27 16:07:08 +0300

    PS-1901: phpcs fix

--- Bundles/NewsletterWidget/src/SprykerShop/Yves/NewsletterWidget/Form/NewsletterSubscriptionForm.php
+++ Bundles/NewsletterWidget/src/SprykerShop/Yves/NewsletterWidget/Form/NewsletterSubscriptionForm.php
@@ -11,8 +11,8 @@ use Spryker\Yves\Kernel\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
-use Symfony\Component\Validator\Constraints\NotBlank;
 use Symfony\Component\Validator\Constraints\Email;
+use Symfony\Component\Validator\Constraints\NotBlank;
 
 class NewsletterSubscriptionForm extends AbstractType
 {
