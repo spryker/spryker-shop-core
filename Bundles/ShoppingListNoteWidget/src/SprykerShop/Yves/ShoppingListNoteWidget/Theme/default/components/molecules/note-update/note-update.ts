4aedf7aba8505cd8ff18e59a52d146312ec1bf34
bezkorovaina <irina.bezkorovayna@spryker.com>
2018-08-31 16:49:04 +0300

    PS-4556: added focus on field

--- Bundles/ShoppingListNoteWidget/src/SprykerShop/Yves/ShoppingListNoteWidget/Theme/default/components/molecules/note-update/note-update.ts
+++ Bundles/ShoppingListNoteWidget/src/SprykerShop/Yves/ShoppingListNoteWidget/Theme/default/components/molecules/note-update/note-update.ts
@@ -4,12 +4,18 @@ export default class ShoppingListNote extends Component {
     button: HTMLFormElement
     noteTextFieldWrapper: HTMLElement
     hiddenClass: string
+    textarea: HTMLFormElement
 
     protected readyCallback(): void {
         this.button = <HTMLFormElement>this.querySelector(`.${this.jsName}`);
         this.noteTextFieldWrapper = <HTMLFormElement>this.querySelector(`.${this.jsName}__wrapper`);
+        this.textarea = <HTMLFormElement>this.querySelector(`.${this.jsName}__item-note`);
         this.hiddenClass = 'is-hidden';
         this.mapEvents();
+    }
+
+    onFocusForm(): void {
+        this.textarea.focus();
     }
 
     protected mapEvents(): void {
@@ -18,6 +24,8 @@ export default class ShoppingListNote extends Component {
                 e.preventDefault();
                 this.button.classList.add(this.hiddenClass);
                 this.noteTextFieldWrapper.classList.remove(this.hiddenClass);
+                this.onFocusForm();
+                console.log( this.textarea)
             })
         }
     }
