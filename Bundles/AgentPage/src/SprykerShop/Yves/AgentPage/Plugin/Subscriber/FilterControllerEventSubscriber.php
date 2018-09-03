57a7c99120e4c664d7e2869faa00b291463e67d6
denys.sokolov <denys.sokolov@spryker.com>
2018-09-03 14:43:28 +0300

    remove unused redirect handler , expand route rules

--- Bundles/AgentPage/src/SprykerShop/Yves/AgentPage/Plugin/Subscriber/FilterControllerEventSubscriber.php
+++ Bundles/AgentPage/src/SprykerShop/Yves/AgentPage/Plugin/Subscriber/FilterControllerEventSubscriber.php
@@ -10,7 +10,6 @@ namespace SprykerShop\Yves\AgentPage\Plugin\Subscriber;
 use Spryker\Yves\Kernel\AbstractPlugin;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 use Symfony\Component\HttpKernel\Event\FilterControllerEvent;
-use Symfony\Component\HttpKernel\Event\GetResponseEvent;
 use Symfony\Component\HttpKernel\KernelEvents;
 
 /**
@@ -25,7 +24,6 @@ class FilterControllerEventSubscriber extends AbstractPlugin implements EventSub
     {
         return [
             KernelEvents::CONTROLLER => 'onKernelController',
-            KernelEvents::EXCEPTION => 'onKernelRequest',
         ];
     }
 
@@ -40,16 +38,6 @@ class FilterControllerEventSubscriber extends AbstractPlugin implements EventSub
     }
 
     /**
-     * @param \Symfony\Component\HttpKernel\Event\GetResponseEvent $event
-     *
-     * @return void
-     */
-    public function onKernelRequest(GetResponseEvent $event)
-    {
-        $this->redirectToLogin($event);
-    }
-
-    /**
      * @return void
      */
     protected function changeUser(): void
@@ -57,17 +45,5 @@ class FilterControllerEventSubscriber extends AbstractPlugin implements EventSub
         $this->getFactory()
             ->createUserChanger()
             ->change();
-    }
-
-    /**
-     * @param \Symfony\Component\HttpKernel\Event\GetResponseEvent $event
-     *
-     * @return void
-     */
-    protected function redirectToLogin(GetResponseEvent $event): void
-    {
-        $this->getFactory()
-            ->createAgentRedirectHandler()
-            ->redirect($event);
     }
 }
