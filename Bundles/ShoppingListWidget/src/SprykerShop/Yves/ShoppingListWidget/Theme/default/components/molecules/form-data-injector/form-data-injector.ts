aefe4915dfd5a7a305bcdc6b8538921c3581bf03
galitsky <eugen.galitsky@spryker.com>
2018-09-04 19:52:36 +0300

    PS-4523 implemented form-data-injector component

---
+++ Bundles/ShoppingListWidget/src/SprykerShop/Yves/ShoppingListWidget/Theme/default/components/molecules/form-data-injector/form-data-injector.ts
@@ -0,0 +1,55 @@
+import Component from 'ShopUi/models/component';
+
+export default class FormDataInjector extends Component {
+    injectFields: HTMLElement[];
+    formForInject: HTMLFormElement;
+    formTakeOutFrom: HTMLFormElement;
+
+    protected readyCallback(): void {
+        this.formForInject = this.closest('form');
+        this.formTakeOutFrom = document.querySelector(this.getFormSelector);
+
+        this.mapEvents();
+    }
+
+    protected mapEvents(): void {
+        this.formForInject.addEventListener('submit', event => this.submitHandle(event), false);
+    }
+
+    private submitHandle(event): void {
+        event.preventDefault();
+
+        this.disableButton();
+        this.addHiddenInputs();
+        this.formForInject.submit();
+    }
+
+    private disableButton(): void {
+        this.formForInject.querySelector('[type="submit"]').setAttribute('disabled', 'disabled');
+    }
+
+    private addHiddenInputs(): void {
+        const fieldsArray: HTMLFormElement[] = Array.from(this.formTakeOutFrom.querySelectorAll(this.getFieldsSelector));
+
+        this.innerHTML = '';
+        fieldsArray.forEach(field => this.addHiddenField(field));
+    }
+
+    private addHiddenField(field): void {
+        let insertField = document.createElement('input');
+
+        insertField.setAttribute('type', 'hidden');
+        insertField.setAttribute('name', field.name);
+        insertField.setAttribute('value', field.value);
+
+        this.appendChild(insertField);
+    }
+
+    get getFormSelector() {
+        return this.getAttribute('form-class');
+    }
+
+    get getFieldsSelector() {
+        return this.getAttribute('fields-selector');
+    }
+}
