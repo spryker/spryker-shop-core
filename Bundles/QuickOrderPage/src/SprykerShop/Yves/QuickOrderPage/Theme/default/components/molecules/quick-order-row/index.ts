fd2f8b89bc6c27edc5116d5ecec115a6a512afa6
Sergey <sergeirybalko@gmail.com>
2018-10-26 08:54:32 +0300

    PS-4233: Add quantity validation

--- Bundles/QuickOrderPage/src/SprykerShop/Yves/QuickOrderPage/Theme/default/components/molecules/quick-order-row/index.ts
+++ Bundles/QuickOrderPage/src/SprykerShop/Yves/QuickOrderPage/Theme/default/components/molecules/quick-order-row/index.ts
@@ -1,2 +1,3 @@
+import './quick-order-row.scss';
 import register from 'ShopUi/app/registry';
 export default register('quick-order-row', () => import(/* webpackMode: "lazy" */'./quick-order-row'));
--- Bundles/QuickOrderPage/src/SprykerShop/Yves/QuickOrderPage/Theme/default/components/molecules/quick-order-row/quick-order-row.ts
+++ Bundles/QuickOrderPage/src/SprykerShop/Yves/QuickOrderPage/Theme/default/components/molecules/quick-order-row/quick-order-row.ts
@@ -7,6 +7,7 @@ export default class QuickOrderRow extends Component {
     ajaxProvider: AjaxProvider;
     autocompleteInput: AutocompleteForm;
     quantityInput: HTMLInputElement;
+    errorMessage: HTMLElement;
 
     protected readyCallback(): void {
         this.ajaxProvider = <AjaxProvider>this.querySelector(`.${this.jsName}__provider`);
@@ -17,12 +18,27 @@ export default class QuickOrderRow extends Component {
 
     protected registerQuantityInput(): void {
         this.quantityInput = <HTMLInputElement>this.querySelector(`.${this.jsName}__quantity, .${this.jsName}-partial__quantity`);
+        this.errorMessage = <HTMLElement>this.querySelector(`.${this.name}__error, .${this.name}-partial__error`);
+
+        if(this.quantityInput) {
+            this.quantityInputAttributes(this.quantityInput);
+        }
     }
 
     protected mapEvents(): void {
         this.autocompleteInput.addEventListener(AutocompleteEvents.SET, (e: CustomEvent) => this.onAutocompleteSet(e));
         this.autocompleteInput.addEventListener(AutocompleteEvents.UNSET, (e: CustomEvent) => this.onAutocompleteUnset(e));
         this.quantityInput.addEventListener('input', debounce((e: Event) => this.onQuantityChange(e), this.autocompleteInput.debounceDelay));
+    }
+
+    protected quantityInputAttributes(quantityInput: HTMLInputElement): void {
+        const max = quantityInput.getAttribute('max'),
+              min = quantityInput.getAttribute('min') || '1',
+              step = quantityInput.getAttribute('step') || '1';
+
+        quantityInput.setAttribute('max', max);
+        quantityInput.setAttribute('min', min);
+        quantityInput.setAttribute('step', step);
     }
 
     protected onAutocompleteSet(e: CustomEvent) {
@@ -38,7 +54,20 @@ export default class QuickOrderRow extends Component {
         this.reloadField(this.autocompleteInput.inputValue, quantity);
     }
 
+    protected toggleErrorMessage(isShow: boolean): void {
+        if (isShow)  {
+            const errorMessageClass = this.errorMessage.classList[0] + '--show';
+            this.errorMessage.classList.add(errorMessageClass);
+
+            setTimeout(() => this.errorMessage.classList.remove(errorMessageClass), 5000);
+        }
+    }
+
     async reloadField(sku: string = '', quantity: number = null) {
+        const quantityMax = this.quantityInput.getAttribute('max'),
+              quantityValue = this.quantityInput.value,
+              isShowErrorMessage = (quantityMax !== '' && quantityValue > quantityMax ) ? true : false;
+
         if (!!sku) {
             this.ajaxProvider.queryParams.set('sku', sku);
             this.ajaxProvider.queryParams.set('index', this.ajaxProvider.getAttribute('class').split('-').pop().trim());
@@ -50,6 +79,7 @@ export default class QuickOrderRow extends Component {
 
         await this.ajaxProvider.fetch();
         this.registerQuantityInput();
+        this.toggleErrorMessage(isShowErrorMessage);
         this.mapEvents();
     }
 }
