501bbd0fbf61b0c156998c20830e1686d906a1d2
Pavlo Asaulenko <asaulenko.pavel@gmail.com>
2018-11-13 17:32:50 +0200

    CC-616 Implement CategoryImageStorageWidget module

---
+++ Bundles/CategoryImageStorageWidget/src/SprykerShop/Yves/CategoryImageStorageWidget/CategoryImageStorageWidgetDependencyProvider.php
@@ -0,0 +1,45 @@
+<?php
+
+/**
+ * Copyright © 2016-present Spryker Systems GmbH. All rights reserved.
+ * Use of this software requires acceptance of the Evaluation License Agreement. See LICENSE file.
+ */
+
+namespace SprykerShop\Yves\CategoryImageStorageWidget;
+
+use Spryker\Yves\Kernel\AbstractBundleDependencyProvider;
+use Spryker\Yves\Kernel\Container;
+use SprykerShop\Yves\CategoryImageStorageWidget\Dependency\CategoryImageStorageWidgetToCategoryImageStorageClientBridge;
+
+class CategoryImageStorageWidgetDependencyProvider extends AbstractBundleDependencyProvider
+{
+    public const CLIENT_CATEGORY_IMAGE_STORAGE = 'CLIENT_CATEGORY_IMAGE_STORAGE';
+
+    /**
+     * @param \Spryker\Yves\Kernel\Container $container
+     *
+     * @return \Spryker\Yves\Kernel\Container
+     */
+    public function provideDependencies(Container $container)
+    {
+        $container = $this->addCategoryImageStorageClient($container);
+
+        return $container;
+    }
+
+    /**
+     * @param \Spryker\Yves\Kernel\Container $container
+     *
+     * @return \Spryker\Yves\Kernel\Container
+     */
+    protected function addCategoryImageStorageClient(Container $container)
+    {
+        $container[static::CLIENT_CATEGORY_IMAGE_STORAGE] = function (Container $container) {
+            return new CategoryImageStorageWidgetToCategoryImageStorageClientBridge(
+                $container->getLocator()->categoryImageStorage()->client()
+            );
+        };
+
+        return $container;
+    }
+}
