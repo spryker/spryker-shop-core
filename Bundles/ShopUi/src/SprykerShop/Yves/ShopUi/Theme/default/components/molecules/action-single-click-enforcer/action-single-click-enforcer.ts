a54d100b0c3afa02e28eb60231d996cf7bda8675
Yuriy Gerton <yuriy.gerton@spryker.com>
2019-01-03 12:02:30 +0200

    TE-1020: fex PR

--- Bundles/ShopUi/src/SprykerShop/Yves/ShopUi/Theme/default/components/molecules/action-single-click-enforcer/action-single-click-enforcer.ts
+++ Bundles/ShopUi/src/SprykerShop/Yves/ShopUi/Theme/default/components/molecules/action-single-click-enforcer/action-single-click-enforcer.ts
@@ -17,7 +17,7 @@ export default class ActionSingleClickEnforcer extends Component {
     protected onTargetClick(event: Event): void {
         const targetElement = <HTMLElement> event.currentTarget;
         const isDisabled: boolean = targetElement.hasAttribute('disabled');
-        const isSubmit: boolean = (<HTMLFormElement>targetElement).type === 'submit';
+        const isSubmit: boolean = (<HTMLInputElement>targetElement).type === 'submit';
 
         if (isDisabled) {
             event.preventDefault();
