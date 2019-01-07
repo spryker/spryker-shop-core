8b4ecda84f6196707d5477bc2d1b26f203c3aff1
Anton Smarovydlo <anton.smarovydlo@spryker.com>
2019-01-07 19:51:07 +0200

    PS-4362: implement proceed to checkout widget & plugins check functionality

---
+++ Bundles/CheckoutWidget/src/SprykerShop/Yves/CheckoutWidget/Dependency/Client/CheckoutWidgetToCheckoutClientBridge.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Copyright © 2016-present Spryker Systems GmbH. All rights reserved.
+ * Use of this software requires acceptance of the Evaluation License Agreement. See LICENSE file.
+ */
+
+namespace SprykerShop\Yves\CheckoutWidget\Dependency\Client;
+
+use Generated\Shared\Transfer\QuoteTransfer;
+
+class CheckoutWidgetToCheckoutClientBridge implements CheckoutWidgetToCheckoutClientInterface
+{
+    /**
+     * @var \Spryker\Client\Checkout\CheckoutClientInterface
+     */
+    protected $checkoutClient;
+
+    /**
+     * @param \Spryker\Client\Checkout\CheckoutClientInterface $checkoutClient
+     */
+    public function __construct($checkoutClient)
+    {
+        $this->checkoutClient = $checkoutClient;
+    }
+
+    /**
+     * @param \Generated\Shared\Transfer\QuoteTransfer $quoteTransfer
+     *
+     * @return bool
+     */
+    public function isQuoteApplicableForCheckout(QuoteTransfer $quoteTransfer): bool
+    {
+        return $this->checkoutClient->isQuoteApplicableForCheckout($quoteTransfer);
+    }
+}
