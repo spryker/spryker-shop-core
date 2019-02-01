147b38580a085ff780064e5d82de56ae60fa0541
danil.moroz <danil.moroz@spryker.com>
2019-02-01 17:51:50 +0200

    CC-990: Refactor remained widget plugins

---
+++ Bundles/CustomerReorderWidget/src/SprykerShop/Yves/CustomerReorderWidget/Theme/default/components/molecules/customer-reorder-form/customer-reorder-form.ts
@@ -0,0 +1,47 @@
+import Component from 'ShopUi/models/component';
+
+export default class CustomerReorderForm extends Component {
+    /**
+     * Elements enabling/disabling the trigger.
+     */
+    readonly selections: HTMLInputElement[]
+
+    /**
+     * Element enabled/disabled by selections changes.
+     */
+    readonly trigger: HTMLElement
+
+    constructor() {
+        super();
+        this.selections = <HTMLInputElement[]>Array.from(this.querySelectorAll(`.${this.jsName}__selection`));
+        this.trigger = <HTMLElement>this.querySelector(`.${this.jsName}__trigger`);
+    }
+
+    protected readyCallback(): void {
+        this.mapEvents();
+    }
+
+    protected mapEvents(): void {
+        this.selections.forEach((selection: HTMLInputElement) =>
+            selection.addEventListener('change', (event: Event) => this.onSelectionChange(event))
+        );
+    }
+
+    protected onSelectionChange(event: Event): void {
+        const enable = this.selections.some((selection: HTMLInputElement) => selection.checked);
+        this.enableTrigger(enable);
+    }
+
+    /**
+     * Sets or removes the disabled attribute from the trigger element.
+     * @param enable A boolean value for checking if the trigger is available for changing.
+     */
+    enableTrigger(enable: boolean): void {
+        if (enable) {
+            this.trigger.removeAttribute('disabled');
+            return;
+        }
+
+        this.trigger.setAttribute('disabled', 'disabled');
+    }
+}
