653129bee320cd2dafc6a7d2ae78efa588ada3a5
Alexey Kravchenko <alexey.kravchenko@spryker.com>
2019-02-08 14:12:51 +0200

    cc-3068 fix validation

--- Bundles/CustomerPage/src/SprykerShop/Yves/CustomerPage/Form/CheckoutAddressItemForm.php
+++ Bundles/CustomerPage/src/SprykerShop/Yves/CustomerPage/Form/CheckoutAddressItemForm.php
@@ -88,7 +88,7 @@ class CheckoutAddressItemForm extends AbstractType
                     return [CheckoutAddressCollectionForm::GROUP_SHIPPING_ADDRESS];
                 }
 
-                return true;
+                return false;
             },
             CheckoutAddressForm::OPTION_VALIDATION_GROUP => CheckoutAddressCollectionForm::GROUP_SHIPPING_ADDRESS,
             CheckoutAddressForm::OPTION_ADDRESS_CHOICES => $options[static::OPTION_ADDRESS_CHOICES],
