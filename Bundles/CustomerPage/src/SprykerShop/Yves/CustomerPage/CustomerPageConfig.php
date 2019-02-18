77d841a80695803e17abbff5e8f8fa643261e4fb
Ruslan Salii <ruslan.salii@spryker.com>
2019-02-18 18:12:20 +0200

    added RegisterForm password constraints

--- Bundles/CustomerPage/src/SprykerShop/Yves/CustomerPage/CustomerPageConfig.php
+++ Bundles/CustomerPage/src/SprykerShop/Yves/CustomerPage/CustomerPageConfig.php
@@ -12,7 +12,12 @@ use Spryker\Yves\Kernel\AbstractBundleConfig;
 
 class CustomerPageConfig extends AbstractBundleConfig
 {
-    protected const MIN_LENGTH_CUSTOMER_PASSWORD = 1;
+    protected const MIN_LENGTH_CUSTOMER_PASSWORD = 6;
+
+    /**
+     * @var int restriction is based on symfony BCryptPasswordEncoder limitation
+     */
+    protected const MAX_LENGTH_CUSTOMER_PASSWORD = 72;
 
     /**
      * @return string
@@ -28,5 +33,13 @@ class CustomerPageConfig extends AbstractBundleConfig
     public function getCustomerPasswordMinLength(): int
     {
         return static::MIN_LENGTH_CUSTOMER_PASSWORD;
+    }
+
+    /**
+     * @return int
+     */
+    public function getCustomerPasswordMaxLength(): int
+    {
+        return static::MAX_LENGTH_CUSTOMER_PASSWORD;
     }
 }
