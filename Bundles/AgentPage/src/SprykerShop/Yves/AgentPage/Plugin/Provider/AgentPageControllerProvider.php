ee355f1eb13796fe097548f4243370c065040847
Karoly Gerner <30629375+gerner-spryker@users.noreply.github.com>
2019-02-28 10:27:48 +0100

    PS-6814: Adjusting comment

--- Bundles/AgentPage/src/SprykerShop/Yves/AgentPage/Plugin/Provider/AgentPageControllerProvider.php
+++ Bundles/AgentPage/src/SprykerShop/Yves/AgentPage/Plugin/Provider/AgentPageControllerProvider.php
@@ -56,6 +56,8 @@ class AgentPageControllerProvider extends AbstractYvesControllerProvider
     }
 
     /**
+     * @uses AgentController::indexAction()
+     *
      * @return $this
      */
     protected function addOverviewRoute()
