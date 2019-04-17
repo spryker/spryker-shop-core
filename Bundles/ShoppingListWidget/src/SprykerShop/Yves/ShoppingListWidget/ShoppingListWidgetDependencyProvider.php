4847e3a0a44c09ee3822b6c6d7fadcaef20b29f4
Alexey Kravchenko <alexey.kravchenko@spryker.com>
2019-04-17 17:59:56 +0300

    cc-4335 add restrictions to shopping list

--- Bundles/ShoppingListWidget/src/SprykerShop/Yves/ShoppingListWidget/ShoppingListWidgetDependencyProvider.php
+++ Bundles/ShoppingListWidget/src/SprykerShop/Yves/ShoppingListWidget/ShoppingListWidgetDependencyProvider.php
@@ -9,12 +9,16 @@ namespace SprykerShop\Yves\ShoppingListWidget;
 
 use Spryker\Yves\Kernel\AbstractBundleDependencyProvider;
 use Spryker\Yves\Kernel\Container;
+use SprykerShop\Yves\ShoppingListWidget\Dependency\Client\ShoppingListWidgetToAvailabilityClientBridge;
 use SprykerShop\Yves\ShoppingListWidget\Dependency\Client\ShoppingListWidgetToCustomerClientBridge;
+use SprykerShop\Yves\ShoppingListWidget\Dependency\Client\ShoppingListWidgetToProductQuantityStorageClientBridge;
 use SprykerShop\Yves\ShoppingListWidget\Dependency\Client\ShoppingListWidgetToShoppingListClientBridge;
 use SprykerShop\Yves\ShoppingListWidget\Dependency\Client\ShoppingListWidgetToShoppingListSessionClientBridge;
 
 class ShoppingListWidgetDependencyProvider extends AbstractBundleDependencyProvider
 {
+    public const CLIENT_AVAILABILITY = 'CLIENT_AVAILABILITY';
+    public const CLIENT_PRODUCT_QUANTITY_STORAGE = 'CLIENT_PRODUCT_QUANTITY_STORAGE';
     public const CLIENT_SHOPPING_LIST = 'CLIENT_SHOPPING_LIST';
     public const CLIENT_CUSTOMER = 'CLIENT_CUSTOMER';
     public const CLIENT_SHOPPING_LIST_SESSION = 'CLIENT_SHOPPING_LIST_SESSION';
@@ -29,6 +33,40 @@ class ShoppingListWidgetDependencyProvider extends AbstractBundleDependencyProvi
         $container = $this->addShoppingListClient($container);
         $container = $this->addCustomerClient($container);
         $container = $this->addShoppingListSessionClient($container);
+        $container = $this->addAvailabilityClient($container);
+        $container = $this->addProductQuantityStorageClient($container);
+
+        return $container;
+    }
+
+    /**
+     * @param \Spryker\Yves\Kernel\Container $container
+     *
+     * @return \Spryker\Yves\Kernel\Container
+     */
+    protected function addProductQuantityStorageClient(Container $container): Container
+    {
+        $container[static::CLIENT_PRODUCT_QUANTITY_STORAGE] = function (Container $container) {
+            return new ShoppingListWidgetToProductQuantityStorageClientBridge(
+                $container->getLocator()->productQuantityStorage()->client()
+            );
+        };
+
+        return $container;
+    }
+
+    /**
+     * @param \Spryker\Yves\Kernel\Container $container
+     *
+     * @return \Spryker\Yves\Kernel\Container
+     */
+    protected function addAvailabilityClient(Container $container): Container
+    {
+        $container[static::CLIENT_AVAILABILITY] = function (Container $container) {
+            return new ShoppingListWidgetToAvailabilityClientBridge(
+                $container->getLocator()->availability()->client()
+            );
+        };
 
         return $container;
     }
---
+++ Bundles/ShoppingListWidget/src/SprykerShop/Yves/ShoppingListWidget/Widget/ShoppingListItemWidget.php
@@ -0,0 +1,98 @@
+<?php
+
+/**
+ * Copyright © 2016-present Spryker Systems GmbH. All rights reserved.
+ * Use of this software requires acceptance of the Evaluation License Agreement. See LICENSE file.
+ */
+
+namespace SprykerShop\Yves\ShoppingListWidget\Widget;
+
+use Generated\Shared\Transfer\ProductConcreteAvailabilityRequestTransfer;
+use Generated\Shared\Transfer\ProductConcreteAvailabilityTransfer;
+use Generated\Shared\Transfer\ProductQuantityStorageTransfer;
+use Generated\Shared\Transfer\ProductViewTransfer;
+use Spryker\Yves\Kernel\Widget\AbstractWidget;
+
+/**
+ * @method \SprykerShop\Yves\ShoppingListWidget\ShoppingListWidgetFactory getFactory()
+ */
+class ShoppingListItemWidget extends AbstractWidget
+{
+    /**
+     * @param \Generated\Shared\Transfer\ProductViewTransfer $productViewTransfer
+     * @param bool $isItemAvailable
+     */
+    public function __construct(ProductViewTransfer $productViewTransfer, bool $isItemAvailable)
+    {
+        $this->addParameter('item', $productViewTransfer)
+            ->addParameter('readOnly', !$isItemAvailable);
+        $this->setQuantityRestrictions($productViewTransfer);
+    }
+
+    /**
+     * @param \Generated\Shared\Transfer\ProductViewTransfer $productViewTransfer
+     *
+     * @return void
+     */
+    protected function setQuantityRestrictions(ProductViewTransfer $productViewTransfer): void
+    {
+        $productQuantityStorageTransfer = $this->getProductQuantityStorageTransfer($productViewTransfer);
+        $productConcreteAvailabilityTransfer = $this->getProductConcreteAvailabilityTransfer($productViewTransfer);
+        $minQuantity = $this->getFactory()
+            ->createQuantityRestrictionReader()
+            ->getMinQuantity($productQuantityStorageTransfer);
+        $maxQuantity = $this->getFactory()
+            ->createQuantityRestrictionReader()
+            ->getMaxQuantity($productQuantityStorageTransfer, $productConcreteAvailabilityTransfer);
+        $quantityInterval = $this->getFactory()
+            ->createQuantityRestrictionReader()
+            ->getQuantityInterval($productQuantityStorageTransfer);
+
+        $this->addParameter('minQuantity', $minQuantity)
+            ->addParameter('maxQuantity', $maxQuantity)
+            ->addParameter('quantityInterval', $quantityInterval);
+    }
+
+    /**
+     * @param \Generated\Shared\Transfer\ProductViewTransfer $productViewTransfer
+     *
+     * @return \Generated\Shared\Transfer\ProductQuantityStorageTransfer|null
+     */
+    protected function getProductQuantityStorageTransfer(ProductViewTransfer $productViewTransfer): ?ProductQuantityStorageTransfer
+    {
+        return $this->getFactory()
+            ->getProductQuantityStorageClient()
+            ->findProductQuantityStorage($productViewTransfer->getIdProductConcrete());
+    }
+
+    /**
+     * @param \Generated\Shared\Transfer\ProductViewTransfer $productViewTransfer
+     *
+     * @return \Generated\Shared\Transfer\ProductConcreteAvailabilityTransfer|null
+     */
+    protected function getProductConcreteAvailabilityTransfer(ProductViewTransfer $productViewTransfer): ?ProductConcreteAvailabilityTransfer
+    {
+        $productConcreteAvailabilityRequestTransfer = new ProductConcreteAvailabilityRequestTransfer();
+        $productConcreteAvailabilityRequestTransfer->setSku($productViewTransfer->getSku());
+
+        return $this->getFactory()
+            ->getAvailabilityClient()
+            ->findProductConcreteAvailability($productConcreteAvailabilityRequestTransfer);
+    }
+
+    /**
+     * @return string
+     */
+    public static function getName(): string
+    {
+        return 'ShoppingListItemWidget';
+    }
+
+    /**
+     * @return string
+     */
+    public static function getTemplate(): string
+    {
+        return '@ShoppingListWidget/views/shopping-list-item/shopping-list-item.twig';
+    }
+}
