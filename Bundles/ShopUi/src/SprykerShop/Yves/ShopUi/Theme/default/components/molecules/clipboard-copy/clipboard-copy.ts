ff8d5243a9b08bb9fecc8b4ae99ce696b67f2c65
zamlelyi <nikolay.zamlely@spryker.com>
2019-04-24 11:54:40 +0300

    PS-9152: Made fixes for clipboard-copy.ts after CR

--- Bundles/ShopUi/src/SprykerShop/Yves/ShopUi/Theme/default/components/molecules/clipboard-copy/clipboard-copy.ts
+++ Bundles/ShopUi/src/SprykerShop/Yves/ShopUi/Theme/default/components/molecules/clipboard-copy/clipboard-copy.ts
@@ -5,7 +5,7 @@ const HIDE_CLASS_NAME = 'is-hidden';
 type Target = HTMLInputElement | HTMLTextAreaElement;
 
 /**
- * @event copy An event emitted when the component performs a copy.
+ * @event copyToClipboard An event emitted when the component performs a copy.
  */
 export default class ClipboardCopy extends Component {
     protected target: Target;
@@ -13,7 +13,6 @@ export default class ClipboardCopy extends Component {
     protected successCopyMessage: HTMLElement;
     protected errorCopyMessage: HTMLElement;
     protected durationTimeoutId: number;
-    protected isCopyCommandSupported: boolean;
 
     /**
      * Default message show duration.
@@ -25,7 +24,6 @@ export default class ClipboardCopy extends Component {
         this.target = <Target>document.querySelector(`${this.targetSelector}`);
         this.successCopyMessage = <HTMLElement>this.querySelector(`.${this.jsName}__success-message`);
         this.errorCopyMessage = <HTMLElement>this.querySelector(`.${this.jsName}__error-message`);
-        this.isCopyCommandSupported = document.queryCommandSupported('copy');
         this.mapEvents();
     }
 
@@ -41,27 +39,31 @@ export default class ClipboardCopy extends Component {
      * Performs the copy to the clipboard and tells the component to show the message.
      */
     copy(): void {
-        if(this.isCopyCommandSupported) {
-            this.target.select();
-            document.execCommand('copy');
-            setTimeout(() => this.showMessageFor(this.successCopyMessage, this.defaultDuration));
-            this.dispatchCustomEvent(EVENT_COPY);
-        } else {
-            setTimeout(() => this.showMessageFor(this.errorCopyMessage, this.defaultDuration));
+        if(!this.isCopyCommandSupported) {
+            setTimeout(() => this.showMessage(this.errorCopyMessage, this.defaultDuration));
+
+            return;
         }
+
+        this.target.select();
+        document.execCommand('copy');
+        setTimeout(() => this.showMessage(this.successCopyMessage, this.defaultDuration));
+        this.dispatchCustomEvent(EVENT_COPY);
     }
 
     /**
      * Shows the message during the time set.
+     * @param message A HTMLElement which contains the text message.
      * @param duration A number value which defines the period of time for which the message is shown.
      */
-    showMessageFor(message: HTMLElement, duration: number): void {
+    showMessage(message: HTMLElement, duration: number): void {
         message.classList.remove(HIDE_CLASS_NAME);
         this.durationTimeoutId = setTimeout(() => this.hideMessage(message), duration);
     }
 
     /**
      * Hides the message.
+     * @param message A HTMLElement which contains the text message.
      */
     hideMessage(message: HTMLElement): void {
         clearTimeout(this.durationTimeoutId);
@@ -80,5 +82,12 @@ export default class ClipboardCopy extends Component {
      */
     get targetSelector(): string {
         return this.getAttribute('target-selector');
+    }
+
+    /**
+     * Gets if a browser supports the automatically copy to clipboard feature.
+     */
+    get isCopyCommandSupported(): boolean {
+        return document.queryCommandSupported('copy');
     }
 }
