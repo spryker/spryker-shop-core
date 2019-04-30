d83bc239cb31cda9dfdf61667ff36489a46e0730
Alexey Kravchenko <alexey.kravchenko@spryker.com>
2019-04-30 13:48:29 +0300

    cc-4650 add docblock to regexp const

--- Bundles/QuickOrderPage/src/SprykerShop/Yves/QuickOrderPage/Form/Constraint/TextOrderFormatConstraintValidator.php
+++ Bundles/QuickOrderPage/src/SprykerShop/Yves/QuickOrderPage/Form/Constraint/TextOrderFormatConstraintValidator.php
@@ -13,8 +13,11 @@ use Symfony\Component\Validator\ConstraintValidator;
 
 class TextOrderFormatConstraintValidator extends ConstraintValidator
 {
+    /**
+     * Checks format of row. It must be <sku><separator><quantity>. Quantity can be float.
+     */
     protected const ROW_REGEXP = "/\w[%s]([0-9]*[.])?[0-9]+$/";
-    
+
     /**
      * @param mixed $textOrder The value that should be validated
      * @param \Symfony\Component\Validator\Constraint|\SprykerShop\Yves\QuickOrderPage\Form\Constraint\TextOrderFormatConstraint $constraint The constraint for the validation
