4e9fd9e0f5970446a7ed3d8088ab5d0906096286
Anton Zubariev <anton.zubariev@spryker.com>
2019-05-13 10:29:57 +0300

    [CMS-1942] Added tests

---
+++ Bundles/ContentProductSetWidget/tests/SprykerShopTest/Yves/ContentProductSetWidget/Plugin/Twig/ContentProductSetTwigPluginTest.php
@@ -0,0 +1,212 @@
+<?php
+
+/**
+ * Copyright © 2016-present Spryker Systems GmbH. All rights reserved.
+ * Use of this software requires acceptance of the Evaluation License Agreement. See LICENSE file.
+ */
+
+namespace SprykerShopTest\Yves\ContentProductSetWidget\Plugin\Twig;
+
+use Codeception\Test\Unit;
+use Generated\Shared\Transfer\ContentProductSetTypeTransfer;
+use Generated\Shared\Transfer\ContentTypeContextTransfer;
+use Generated\Shared\Transfer\ProductSetDataStorageTransfer;
+use Generated\Shared\Transfer\ProductViewTransfer;
+use ReflectionClassConstant;
+use Spryker\Client\ContentProductSet\Exception\InvalidProductSetTermException;
+use Spryker\Service\Container\ContainerInterface;
+use Spryker\Shared\Kernel\Store;
+use SprykerShop\Yves\ContentProductSetWidget\ContentProductSetWidgetDependencyProvider;
+use SprykerShop\Yves\ContentProductSetWidget\Dependency\Client\ContentProductSetWidgetToContentProductSetClientInterface;
+use SprykerShop\Yves\ContentProductSetWidget\Dependency\Client\ContentProductSetWidgetToProductSetStorageClientInterface;
+use SprykerShop\Yves\ContentProductSetWidget\Dependency\Client\ContentProductSetWidgetToProductStorageClientInterface;
+use SprykerShop\Yves\ContentProductSetWidget\Plugin\Twig\ContentProductSetTwigPlugin;
+use SprykerShop\Yves\ContentProductSetWidget\Twig\ContentProductSetTwigFunction;
+use Twig\Environment;
+use Twig\Loader\FilesystemLoader;
+
+/**
+ * Auto-generated group annotations
+ * @group SprykerShop
+ * @group Yves
+ * @group ContentProductSetWidget
+ * @group Plugin
+ * @group Twig
+ * @group ContentProductSetTwigPluginTest
+ * Add your own group annotations below this line
+ */
+class ContentProductSetTwigPluginTest extends Unit
+{
+    protected const LOCALE = 'de_DE';
+
+    protected const DEFAULT_TEMPLATE = 'default';
+    protected const WRONG_TEMPLATE = 'wrong';
+
+    protected const CONTENT_ID = 0;
+    protected const CONTENT_TERM = 'TERM';
+
+    protected const MESSAGE_CONTENT_PRODUCT_ABSTRACT_LIST_NOT_FOUND = '<strong>Content product set with ID 0 not found.</strong>';
+    protected const MESSAGE_WRONG_CONTENT_PRODUCT_ABSTRACT_LIST_TYPE = '<strong>Content product set widget could not be rendered because the content item with ID 0 is not an abstract product list.</strong>';
+    protected const MESSAGE_NOT_SUPPORTED_TEMPLATE = '<strong>"wrong" is not supported name of template.</strong>';
+    protected const RENDERED_STRING = 'output';
+
+    /**
+     * @var \SprykerShopTest\Yves\ContentProductSetWidget\ContentProductSetWidgetYvesTester
+     */
+    protected $tester;
+
+    /**
+     * @return void
+     */
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        Store::getInstance()->setCurrentLocale(static::LOCALE);
+    }
+
+    /**
+     * @return void
+     */
+    public function testContentProductSetNotFound(): void
+    {
+        // Act
+        $productAbstractContent = call_user_func($this->getContentProductSetTwigPlugin()->getCallable(), static::CONTENT_ID, static::DEFAULT_TEMPLATE);
+
+        // Assert
+        $this->assertEquals(static::MESSAGE_CONTENT_PRODUCT_ABSTRACT_LIST_NOT_FOUND, $productAbstractContent);
+    }
+
+    /**
+     * @return void
+     */
+    public function testContentProductSetWrongType(): void
+    {
+        // Assign
+        $contentTypeContextTransfer = new ContentTypeContextTransfer();
+        $contentTypeContextTransfer->setIdContent(static::CONTENT_ID);
+        $contentTypeContextTransfer->setTerm(static::CONTENT_TERM);
+        $this->setContentProductClientException();
+
+        // Act
+        $productSetContent = call_user_func($this->getContentProductSetTwigPlugin()->getCallable(), static::CONTENT_ID, static::DEFAULT_TEMPLATE);
+
+        // Assert
+        $this->assertEquals(static::MESSAGE_WRONG_CONTENT_PRODUCT_ABSTRACT_LIST_TYPE, $productSetContent);
+    }
+
+    /**
+     * @return void
+     */
+    public function testContentProductSetWrongTemplate(): void
+    {
+        // Assign
+        $contentTypeContextTransfer = new ContentProductSetTypeTransfer();
+        $contentTypeContextTransfer->setIdProductSet(static::CONTENT_ID);
+        $this->setContentProductSetClientReturn($contentTypeContextTransfer);
+        $this->setProductStorageClientReturn();
+
+        // Act
+        $productSetContent = call_user_func($this->getContentProductSetTwigPlugin()->getCallable(), static::CONTENT_ID, static::WRONG_TEMPLATE);
+
+        // Assert
+        $this->assertEquals(static::MESSAGE_NOT_SUPPORTED_TEMPLATE, $productSetContent);
+    }
+
+    /**
+     * @return void
+     */
+    public function testContentProductSetRendering(): void
+    {
+        // Assign
+        $contentTypeContextTransfer = new ContentProductSetTypeTransfer();
+        $contentTypeContextTransfer->setIdProductSet(static::CONTENT_ID);
+
+        $this->setContentProductSetClientReturn($contentTypeContextTransfer);
+        $this->setProductStorageClientReturn();
+        $this->setProductSetStorageClientReturn();
+
+        // Act
+        $productSetContent = call_user_func($this->getContentProductSetTwigPlugin()->getCallable(), static::CONTENT_ID, static::DEFAULT_TEMPLATE);
+
+        // Assert
+        $this->assertEquals(static::RENDERED_STRING, $productSetContent);
+    }
+
+    /**
+     * @return void
+     */
+    protected function setContentProductClientException(): void
+    {
+        $contentProductSetWidgetToContentProductClientBridge = $this->getMockBuilder(ContentProductSetWidgetToContentProductSetClientInterface::class)->getMock();
+        $contentProductSetWidgetToContentProductClientBridge->method('executeProductSetTypeById')->willThrowException(new InvalidProductSetTermException());
+        $this->tester->setDependency(ContentProductSetWidgetDependencyProvider::CLIENT_CONTENT_PRODUCT_SET, $contentProductSetWidgetToContentProductClientBridge);
+    }
+
+    /**
+     * @param \Generated\Shared\Transfer\ContentProductSetTypeTransfer|null $contentTypeContextTransfer
+     *
+     * @return void
+     */
+    protected function setContentProductSetClientReturn(?ContentProductSetTypeTransfer $contentTypeContextTransfer = null): void
+    {
+        $contentProductSetWidgetToContentProductClientBridge = $this->getMockBuilder(ContentProductSetWidgetToContentProductSetClientInterface::class)->getMock();
+        $contentProductSetWidgetToContentProductClientBridge->method('executeProductSetTypeById')->willReturn($contentTypeContextTransfer);
+        $this->tester->setDependency(ContentProductSetWidgetDependencyProvider::CLIENT_CONTENT_PRODUCT_SET, $contentProductSetWidgetToContentProductClientBridge);
+    }
+
+    /**
+     * @return void
+     */
+    protected function setProductSetStorageClientReturn(): void
+    {
+        $contentProductSetWidgetToProductSetStorageClientBridge = $this->getMockBuilder(ContentProductSetWidgetToProductSetStorageClientInterface::class)->getMock();
+        $contentProductSetWidgetToProductSetStorageClientBridge->method('getProductSetByIdProductSet')->willReturn(new ProductSetDataStorageTransfer());
+        $this->tester->setDependency(ContentProductSetWidgetDependencyProvider::CLIENT_PRODUCT_SET_STORAGE, $contentProductSetWidgetToProductSetStorageClientBridge);
+    }
+
+    /**
+     * @return void
+     */
+    protected function setProductStorageClientReturn(): void
+    {
+        $contentProductSetWidgetToProductStorageClientBridge = $this->getMockBuilder(ContentProductSetWidgetToProductStorageClientInterface::class)->getMock();
+        $contentProductSetWidgetToProductStorageClientBridge->method('findProductAbstractStorageData')->willReturn([]);
+        $contentProductSetWidgetToProductStorageClientBridge->method('mapProductAbstractStorageData')->willReturn(new ProductViewTransfer());
+        $this->tester->setDependency(ContentProductSetWidgetDependencyProvider::CLIENT_PRODUCT_STORAGE, $contentProductSetWidgetToProductStorageClientBridge);
+    }
+
+    /**
+     * @return bool|\Twig\TwigFunction
+     */
+    protected function getContentProductSetTwigPlugin()
+    {
+        $functionName = new ReflectionClassConstant(ContentProductSetTwigFunction::class, 'FUNCTION_CONTENT_PRODUCT_SET');
+
+        return $this->getTwig()->getFunction($functionName->getValue());
+    }
+
+    /**
+     * @return \SprykerShop\Yves\ContentProductSetWidget\Plugin\Twig\ContentProductSetTwigPlugin
+     */
+    protected function createTwigPlugin(): ContentProductSetTwigPlugin
+    {
+        return new ContentProductSetTwigPlugin();
+    }
+
+    /**
+     * @return \Twig\Environment
+     */
+    protected function getTwig(): Environment
+    {
+        $twigMock = $this->getMockBuilder(Environment::class)->setConstructorArgs([new FilesystemLoader()])
+            ->setMethods(['render'])
+            ->getMock();
+
+        $twigMock->method('render')->willReturn(static::RENDERED_STRING);
+        $twigPlugin = $this->createTwigPlugin();
+        $twigPlugin->extend($twigMock, $this->getMockBuilder(ContainerInterface::class)->getMock());
+
+        return $twigMock;
+    }
+}
