c25744e9ece25501a75bcd47e89c7db7dbc80f71
Vitaliy Kirichenko <vitaly.kirichenko@spryker.com>
2019-05-22 13:08:54 +0300

    PS-9444: Finished success message functionality implementation

--- Bundles/ResourceSharePage/src/SprykerShop/Yves/ResourceSharePage/Controller/LinkController.php
+++ Bundles/ResourceSharePage/src/SprykerShop/Yves/ResourceSharePage/Controller/LinkController.php
@@ -19,6 +19,9 @@ use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
  */
 class LinkController extends AbstractController
 {
+    protected const MESSAGE_TYPE_ERROR = 'error';
+    protected const MESSAGE_TYPE_SUCCESS = 'success';
+
     /**
      * @see \SprykerShop\Yves\CustomerPage\Plugin\Provider\CustomerPageControllerProvider::ROUTE_LOGIN
      */
@@ -59,7 +62,9 @@ class LinkController extends AbstractController
 
         if ($resourceShareResponseTransfer->getIsLoginRequired()) {
             foreach ($resourceShareResponseTransfer->getMessages() as $messageTransfer) {
-                $this->addErrorMessage($messageTransfer->getValue());
+                if ($messageTransfer->getType() === static::MESSAGE_TYPE_ERROR) {
+                    $this->addErrorMessage($messageTransfer->getValue());
+                }
             }
 
             $routeTransfer = (new RouteTransfer())
@@ -76,10 +81,18 @@ class LinkController extends AbstractController
 
         if (!$resourceShareResponseTransfer->getIsSuccessful()) {
             foreach ($resourceShareResponseTransfer->getMessages() as $messageTransfer) {
-                $this->addErrorMessage($messageTransfer->getValue());
+                if ($messageTransfer->getType() === static::MESSAGE_TYPE_ERROR) {
+                    $this->addErrorMessage($messageTransfer->getValue());
+                }
             }
 
             throw new NotFoundHttpException();
+        }
+
+        foreach ($resourceShareResponseTransfer->getMessages() as $messageTransfer) {
+            if ($messageTransfer->getType() === static::MESSAGE_TYPE_SUCCESS) {
+                $this->addSuccessMessage($messageTransfer->getValue());
+            }
         }
 
         $routeTransfer = $this->getFactory()->createRouteResolver()
