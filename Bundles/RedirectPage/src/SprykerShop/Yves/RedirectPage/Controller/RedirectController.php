3fa585919fc0ddcca8030e29529089377daf2b81
Sergey Samoylov <sergey.samoylov@spryker.com>
2019-05-23 15:21:10 +0300

    cms-2182: add additional headers for 301 redirect cache

--- Bundles/RedirectPage/src/SprykerShop/Yves/RedirectPage/Controller/RedirectController.php
+++ Bundles/RedirectPage/src/SprykerShop/Yves/RedirectPage/Controller/RedirectController.php
@@ -14,6 +14,11 @@ class RedirectController extends AbstractController
 {
     public const STORAGE_CACHE_STRATEGY = StorageConstants::STORAGE_CACHE_STRATEGY_INACTIVE;
 
+    protected const HEADER_PARAMETER_CACHE_CONTROL = 'Cache-Control';
+    protected const HEADER_PARAMETER_CACHE_CONTROL_VALUE = 'no-store, no-cache, must-revalidate, post-check=0, pre-check=0';
+    protected const HEADER_PARAMETER_EXPIRES = 'Expires';
+    protected const STATUS_REDIRECT_301 = 301;
+
     /**
      * @param array $meta
      *
@@ -21,9 +26,17 @@ class RedirectController extends AbstractController
      */
     public function redirectAction($meta)
     {
-        return $this->redirectResponseExternal(
+        $response = $this->redirectResponseExternal(
             $meta['to_url'],
             $meta['status']
         );
+
+        if ($meta['status'] === static::STATUS_REDIRECT_301) {
+            $expires = date('D, d M Y H:i:s e', strtotime('-1 day'));
+            $response->headers->set(static::HEADER_PARAMETER_CACHE_CONTROL, static::HEADER_PARAMETER_CACHE_CONTROL_VALUE);
+            $response->headers->set(static::HEADER_PARAMETER_EXPIRES, $expires);
+        }
+
+        return $response;
     }
 }
