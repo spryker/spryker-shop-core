b9db82de57b48fac967624082f16727e2060d8ad
danil.moroz <danil.moroz@spryker.com>
2019-05-29 14:25:08 +0300

    CC-5057: Checkout: Button 'Go to Shipment' is available when products doesn't have defined addresses

---
+++ Bundles/CheckoutPage/src/SprykerShop/Yves/CheckoutPage/Theme/default/components/molecules/validate-next-checkout-step/validate-next-checkout-step.ts
@@ -0,0 +1,13 @@
+import Component from 'ShopUi/models/component';
+
+export default class ValidateNextCheckoutStep extends Component {
+
+
+    protected readyCallback(): void {
+        this.mapEvents();
+    }
+
+    protected mapEvents(): void {
+
+    }
+}
