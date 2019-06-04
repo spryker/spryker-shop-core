5514f8cd88b9b5ca4820fc491c13591f2afe3518
Yuriy Gerton <yuriy.gerton@spryker.com>
2019-06-04 15:40:23 +0300

    TE-954: add readme file. fix ts.

---
+++ Bundles/ShopUi/src/SprykerShop/Yves/ShopUi/Theme/default/components/molecules/form-submitter/README.md
@@ -0,0 +1,1 @@
+Submits form by event.
--- Bundles/ShopUi/src/SprykerShop/Yves/ShopUi/Theme/default/components/molecules/form-submitter/form-submitter.ts
+++ Bundles/ShopUi/src/SprykerShop/Yves/ShopUi/Theme/default/components/molecules/form-submitter/form-submitter.ts
@@ -1,32 +1,40 @@
 import Component from 'ShopUi/models/component';
 
+const TAG_NAME = 'form';
+
 export default class FormSubmitter extends Component {
-    /**
-     * Elements triggering the form submit.
-     */
     protected triggers: HTMLElement[];
 
-    protected readyCallback(): void {
+    protected readyCallback(): void {}
+
+    /**
+     * Default callback, which is called when all web components are ready for use.
+     */
+    mountCallback(): void {
         this.triggers = <HTMLElement[]>Array.from(document.querySelectorAll(this.triggerSelector));
         this.mapEvents();
     }
 
     protected mapEvents(): void {
         this.triggers.forEach(trigger =>
-            trigger.addEventListener(this.eventName, this.triggerEvent.bind(this, trigger)));
+            trigger.addEventListener(this.eventName, (event: Event) => this.onEvent(event)));
     }
 
-    protected triggerEvent(trigger: HTMLElement): void {
-        const TAG_NAME = 'form';
+    protected onEvent(event: Event): void {
+        const trigger = <HTMLFormElement>event.currentTarget;
         const form = <HTMLFormElement>trigger.closest(TAG_NAME);
+
         if (!form) {
             return;
         }
-        const submit = <HTMLButtonElement | HTMLInputElement>form.querySelector('[type="submit"]') ||
-            <HTMLButtonElement> form.querySelector('button');
+
+        const submit = <HTMLButtonElement | HTMLInputElement>form.querySelector('[type="submit"]')
+            || <HTMLButtonElement>form.querySelector('button');
+
         if (submit) {
             submit.click();
         }
+
         form.submit();
     }
 
