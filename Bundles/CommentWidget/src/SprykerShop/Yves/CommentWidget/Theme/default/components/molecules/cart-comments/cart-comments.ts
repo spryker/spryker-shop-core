1dba191543f75aea4075896c50a49031f9e35945
k.kravchenko <kostiantyn.Kravchenko@spryker.com>
2019-06-11 16:38:33 +0300

    PS-9485: Create basic widget [FE]
    Refactoring code after code review

--- Bundles/CommentWidget/src/SprykerShop/Yves/CommentWidget/Theme/default/components/molecules/cart-comments/cart-comments.ts
+++ Bundles/CommentWidget/src/SprykerShop/Yves/CommentWidget/Theme/default/components/molecules/cart-comments/cart-comments.ts
@@ -4,22 +4,34 @@ export default class CartComments extends Component {
     /**
      * Elements targeted by click action.
      */
-    readonly tabs: HTMLElement[];
+    tabs: HTMLElement[];
     /**
      * Elements shows by click on tab.
      */
-    readonly contentBlocks: HTMLElement[];
+    contentBlocks: HTMLElement[];
     /**
      * Active class for tabs.
      */
+    tabActiveClass: string;
+    /**
+     * Active class for content block.
+     */
+    contentBlockActiveClass: string;
 
     constructor() {
         super();
-        this.tabs = <HTMLElement[]>Array.from(this.querySelectorAll(this.tabSelector));
-        this.contentBlocks = <HTMLElement[]>Array.from(this.querySelectorAll(this.contentBlockSelector));
     }
 
-    protected readyCallback(): void {
+    protected readyCallback(): void {}
+
+    /**
+     * Default callback, which is called when all web components are ready for use.
+     */
+    mountCallback(): void {
+        this.tabs = <HTMLElement[]>Array.from(this.querySelectorAll(`.${this.jsName}__tab`));
+        this.contentBlocks = <HTMLElement[]>Array.from(this.querySelectorAll(`.${this.jsName}__content-item`));
+        this.tabActiveClass = `${this.name}__tab--active`;
+        this.contentBlockActiveClass = `${this.name}__content-item--active`;
         this.mapEvents();
     }
 
@@ -31,48 +43,18 @@ export default class CartComments extends Component {
 
     protected onTabClick(event: Event): void {
         const clickedTab = <HTMLElement>event.target;
-        if (!clickedTab.classList.contains(this.tabClassToToggle)) {
+        if (!clickedTab.classList.contains(this.tabActiveClass)) {
             this.swithOnThisTab(clickedTab);
         }
     }
 
     protected swithOnThisTab(newActiveTab): void {
-        const tabActiveClass = this.tabClassToToggle;
-        const contentBlockActiveClass = this.contentBlockClassToToggle;
-        const currentActiveTab = this.querySelector(`.${tabActiveClass}`);
-        const currentActiveContentBlock = this.querySelector(`.${contentBlockActiveClass}`);
+        const currentActiveTab = this.querySelector(`.${this.tabActiveClass}`);
+        const currentActiveContentBlock = this.querySelector(`.${this.contentBlockActiveClass}`);
         const indexOfNeededContentBlock = this.tabs.findIndex(tab => tab===newActiveTab);
-        currentActiveTab.classList.remove(tabActiveClass);
-        newActiveTab.classList.add(tabActiveClass);
-        currentActiveContentBlock.classList.remove(contentBlockActiveClass);
-        this.contentBlocks[indexOfNeededContentBlock].classList.add(contentBlockActiveClass);
-
-    }
-    /**
-     * Gets a querySelector of the tab element.
-     */
-    get tabSelector(): string {
-        return this.getAttribute('tab-selector');
-    }
-
-    /**
-     * Gets a querySelector of the content block element.
-     */
-    get contentBlockSelector(): string {
-        return this.getAttribute('content-block-selector');
-    }
-
-    /**
-     * Gets a class name for the active tab.
-     */
-    get tabClassToToggle(): string {
-        return this.getAttribute('tab-class-to-toggle');
-    }
-
-    /**
-     * Gets a class name for the active content block.
-     */
-    get contentBlockClassToToggle(): string {
-        return this.getAttribute('content-block-class-to-toggle');
+        currentActiveTab.classList.remove(this.tabActiveClass);
+        newActiveTab.classList.add(this.tabActiveClass);
+        currentActiveContentBlock.classList.remove(this.contentBlockActiveClass);
+        this.contentBlocks[indexOfNeededContentBlock].classList.add(this.contentBlockActiveClass);
     }
 }
