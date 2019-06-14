ed85ab0041a1202d25666b29ef15d525463061f7
Alexey Kravchenko <alexey.kravchenko@spryker.com>
2019-06-14 15:06:43 +0300

    cc-5226 remove dependency

--- Bundles/ProductQuantityWidget/src/SprykerShop/Yves/ProductQuantityWidget/ProductQuantityWidgetDependencyProvider.php
+++ Bundles/ProductQuantityWidget/src/SprykerShop/Yves/ProductQuantityWidget/ProductQuantityWidgetDependencyProvider.php
@@ -10,12 +10,10 @@ namespace SprykerShop\Yves\ProductQuantityWidget;
 use Spryker\Yves\Kernel\AbstractBundleDependencyProvider;
 use Spryker\Yves\Kernel\Container;
 use SprykerShop\Yves\ProductQuantityWidget\Dependency\Client\ProductQuantityWidgetToProductQuantityStorageClientBridge;
-use SprykerShop\Yves\ProductQuantityWidget\Dependency\Service\ProductQuantityWidgetToProductQuantityServiceBridge;
 
 class ProductQuantityWidgetDependencyProvider extends AbstractBundleDependencyProvider
 {
     public const CLIENT_PRODUCT_QUANTITY_STORAGE = 'CLIENT_PRODUCT_QUANTITY_STORAGE';
-    public const SERVICE_PRODUCT_QUANTITY = 'SERVICE_PRODUCT_QUANTITY';
 
     /**
      * @param \Spryker\Yves\Kernel\Container $container
@@ -25,7 +23,6 @@ class ProductQuantityWidgetDependencyProvider extends AbstractBundleDependencyPr
     public function provideDependencies(Container $container): Container
     {
         $container = $this->addProductQuantityStorageClient($container);
-        $container = $this->addProductQuantityService($container);
 
         return $container;
     }
@@ -40,22 +37,6 @@ class ProductQuantityWidgetDependencyProvider extends AbstractBundleDependencyPr
         $container->set(static::CLIENT_PRODUCT_QUANTITY_STORAGE, function (Container $container) {
             return new ProductQuantityWidgetToProductQuantityStorageClientBridge(
                 $container->getLocator()->productQuantityStorage()->client()
-            );
-        });
-
-        return $container;
-    }
-
-    /**
-     * @param \Spryker\Yves\Kernel\Container $container
-     *
-     * @return \Spryker\Yves\Kernel\Container
-     */
-    protected function addProductQuantityService(Container $container): Container
-    {
-        $container->set(static::SERVICE_PRODUCT_QUANTITY, function (Container $container) {
-            return new ProductQuantityWidgetToProductQuantityServiceBridge(
-                $container->getLocator()->productQuantity()->service()
             );
         });
 
