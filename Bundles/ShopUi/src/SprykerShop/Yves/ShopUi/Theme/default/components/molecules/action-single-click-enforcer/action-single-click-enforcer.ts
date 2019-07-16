3d7f5766c5acbea64786cd1794f3dbe5089347c1
danil.moroz <danil.moroz@spryker.com>
2019-07-16 14:46:31 +0300

    CC-5474: Data init single click functionality breaks any other listeners on submit event

--- Bundles/ShopUi/src/SprykerShop/Yves/ShopUi/Theme/default/components/molecules/action-single-click-enforcer/action-single-click-enforcer.ts
+++ Bundles/ShopUi/src/SprykerShop/Yves/ShopUi/Theme/default/components/molecules/action-single-click-enforcer/action-single-click-enforcer.ts
@@ -20,8 +20,9 @@ export default class ActionSingleClickEnforcer extends Component {
     protected onTargetClick(event: Event): void {
         const targetElement = <HTMLElement>event.currentTarget;
         const isDisabled: boolean = targetElement.hasAttribute('disabled');
-        const isSubmit: boolean = (<HTMLInputElement>targetElement).type === 'submit';
-        const form: HTMLFormElement = isSubmit ? targetElement.closest('form') : undefined;
+        const form: HTMLFormElement = targetElement.closest('form');
+        const submit = form ? <HTMLButtonElement | HTMLInputElement>form.querySelector('[type="submit"]')
+            || <HTMLButtonElement>form.getElementsByTagName('button')[0] : null;
 
         if (isDisabled) {
             event.preventDefault();
@@ -29,10 +30,20 @@ export default class ActionSingleClickEnforcer extends Component {
             return;
         }
 
-        if (isSubmit && form) {
-            form.submit();
+        if (form && submit) {
+            submit.click();
+
+            form.addEventListener('submit', () => {
+                this.disableElement(targetElement);
+            });
+
+            return;
         }
 
+        this.disableElement(targetElement);
+    }
+
+    protected disableElement(targetElement: HTMLElement): void {
         targetElement.setAttribute('disabled', 'disabled');
     }
 
