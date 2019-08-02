a0e951f78f67c4134bdaca189c451e9dcb75d2fe
gechetspr <serhii.chepela@spryker.com>
2019-08-02 09:38:53 +0300

    TE-963 added a new WebProfilerFormPlugin

--- Bundles/WebProfilerWidget/src/SprykerShop/Yves/WebProfilerWidget/WebProfilerWidgetFactory.php
+++ Bundles/WebProfilerWidget/src/SprykerShop/Yves/WebProfilerWidget/WebProfilerWidgetFactory.php
@@ -10,6 +10,15 @@ namespace SprykerShop\Yves\WebProfilerWidget;
 use Spryker\Shared\Twig\Loader\FilesystemLoader;
 use Spryker\Shared\Twig\Loader\FilesystemLoaderInterface;
 use Spryker\Yves\Kernel\AbstractFactory;
+use Symfony\Component\Form\Extension\DataCollector\FormDataCollector;
+use Symfony\Component\Form\Extension\DataCollector\FormDataCollectorInterface;
+use Symfony\Component\Form\Extension\DataCollector\FormDataExtractor;
+use Symfony\Component\Form\Extension\DataCollector\FormDataExtractorInterface;
+use Symfony\Component\Form\Extension\DataCollector\Proxy\ResolvedTypeFactoryDataCollectorProxy;
+use Symfony\Component\Form\Extension\DataCollector\Type\DataCollectorTypeExtension;
+use Symfony\Component\Form\FormTypeExtensionInterface;
+use Symfony\Component\Form\ResolvedFormTypeFactory;
+use Symfony\Component\Form\ResolvedFormTypeFactoryInterface;
 
 /**
  * @method \SprykerShop\Yves\WebProfilerWidget\WebProfilerWidgetConfig getConfig()
@@ -30,5 +39,48 @@ class WebProfilerWidgetFactory extends AbstractFactory
     public function createTwigFilesystemLoader(): FilesystemLoaderInterface
     {
         return new FilesystemLoader($this->getConfig()->getWebProfilerTemplatePaths(), 'WebProfiler');
+    }
+
+    /**
+     * @return \Symfony\Component\Form\ResolvedFormTypeFactoryInterface
+     */
+    public function createResolvedTypeFactoryDataCollectorProxy(): ResolvedFormTypeFactoryInterface
+    {
+        return new ResolvedTypeFactoryDataCollectorProxy(
+            $this->createResolvedFormTypeFactory(),
+            $this->createFormDataCollector()
+        );
+    }
+
+    /**
+     * @return \Symfony\Component\Form\ResolvedFormTypeFactoryInterface
+     */
+    public function createResolvedFormTypeFactory(): ResolvedFormTypeFactoryInterface
+    {
+        return new ResolvedFormTypeFactory();
+    }
+
+    /**
+     * @return \Symfony\Component\Form\Extension\DataCollector\FormDataCollectorInterface
+     */
+    public function createFormDataCollector(): FormDataCollectorInterface
+    {
+        return new FormDataCollector($this->createFormDataExtractor());
+    }
+
+    /**
+     * @return \Symfony\Component\Form\Extension\DataCollector\FormDataExtractorInterface
+     */
+    public function createFormDataExtractor(): FormDataExtractorInterface
+    {
+        return new FormDataExtractor();
+    }
+
+    /**
+     * @return \Symfony\Component\Form\FormTypeExtensionInterface
+     */
+    public function createDataCollectorTypeExtension(): FormTypeExtensionInterface
+    {
+        return new DataCollectorTypeExtension($this->createFormDataCollector());
     }
 }
