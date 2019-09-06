f2a67482be6a453cf7a05589d0428e14f32363ab
Dmitriy Svetlichniy <dmitriy.svetlichniy@spryker.com>
2019-09-06 16:48:19 +0300

    CC-6257. Refactoring after next AA CR, part 3
    * Updated Oms version in composer.json files for modules: Availability, AvailabilityGui, Checkout, DummyPayment, OfferGui, ProductPackagingUnit, Sales, SalesReclamationGui, SalesStatistics.
    * Refactored usages of ShipmentConstants to use Shared/ShipmentConfig instead for modules: Checkout, CheckoutPage, CustomerPage, ManualOrderEntryGui, Sales, Shipment, ShipmentCartConnector, ShipmentDiscountConnector, ShipmentGui, ShipmentsRestApi.

--- Bundles/CheckoutPage/src/SprykerShop/Yves/CheckoutPage/Process/Steps/ShipmentStep/PostConditionChecker.php
+++ Bundles/CheckoutPage/src/SprykerShop/Yves/CheckoutPage/Process/Steps/ShipmentStep/PostConditionChecker.php
@@ -10,7 +10,7 @@ namespace SprykerShop\Yves\CheckoutPage\Process\Steps\ShipmentStep;
 use Generated\Shared\Transfer\ExpenseTransfer;
 use Generated\Shared\Transfer\QuoteTransfer;
 use Generated\Shared\Transfer\ShipmentGroupTransfer;
-use SprykerShop\Shared\CheckoutPage\CheckoutPageConstants;
+use SprykerShop\Shared\CheckoutPage\CheckoutPageConfig;
 use SprykerShop\Yves\CheckoutPage\Dependency\Service\CheckoutPageToShipmentServiceInterface;
 use SprykerShop\Yves\CheckoutPage\GiftCard\GiftCardItemsCheckerInterface;
 use SprykerShop\Yves\CheckoutPage\Process\Steps\PostConditionCheckerInterface;
@@ -144,7 +144,7 @@ class PostConditionChecker implements PostConditionCheckerInterface
      */
     protected function checkShipmentExpenseKey(ExpenseTransfer $expenseTransfer, string $itemShipmentKey): bool
     {
-        return $expenseTransfer->getType() === CheckoutPageConstants::SHIPMENT_EXPENSE_TYPE
+        return $expenseTransfer->getType() === CheckoutPageConfig::SHIPMENT_EXPENSE_TYPE
             && $expenseTransfer->getShipment() !== null
             && $this->shipmentService->getShipmentHashKey($expenseTransfer->getShipment()) === $itemShipmentKey;
     }
@@ -163,7 +163,7 @@ class PostConditionChecker implements PostConditionCheckerInterface
         }
 
         foreach ($quoteTransfer->getExpenses() as $expenseTransfer) {
-            if ($expenseTransfer->getType() === CheckoutPageConstants::SHIPMENT_EXPENSE_TYPE) {
+            if ($expenseTransfer->getType() === CheckoutPageConfig::SHIPMENT_EXPENSE_TYPE) {
                 return $quoteTransfer->getShipment()->getShipmentSelection() !== null;
             }
         }
