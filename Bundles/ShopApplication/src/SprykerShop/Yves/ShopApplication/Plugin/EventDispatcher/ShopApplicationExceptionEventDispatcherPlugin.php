d88ceeb758f91da8537e08cc0a6504478cad58ee
René Klatt <rene.klatt@spryker.com>
2019-09-18 07:06:20 +0000

    TE-1190 Deprecated YvesExceptionServiceProvider and added ShopApplicationExceptionEventDispatcherPlugin.

---
+++ Bundles/ShopApplication/src/SprykerShop/Yves/ShopApplication/Plugin/EventDispatcher/ShopApplicationExceptionEventDispatcherPlugin.php
@@ -0,0 +1,55 @@
+<?php
+
+/**
+ * Copyright © 2016-present Spryker Systems GmbH. All rights reserved.
+ * Use of this software requires acceptance of the Evaluation License Agreement. See LICENSE file.
+ */
+
+namespace SprykerShop\Yves\ShopApplication\Plugin\EventDispatcher;
+
+use Spryker\Service\Container\ContainerInterface;
+use Spryker\Shared\EventDispatcher\EventDispatcherInterface;
+use Spryker\Shared\EventDispatcherExtension\Dependency\Plugin\EventDispatcherPluginInterface;
+use Spryker\Yves\Kernel\AbstractPlugin;
+use Symfony\Component\HttpKernel\Event\GetResponseForExceptionEvent;
+use Symfony\Component\HttpKernel\KernelEvents;
+
+/**
+ * @method \SprykerShop\Yves\ShopApplication\ShopApplicationFactory getFactory()
+ * @method \SprykerShop\Yves\ShopApplication\ShopApplicationConfig getConfig()
+ */
+class ShopApplicationExceptionEventDispatcherPlugin extends AbstractPlugin implements EventDispatcherPluginInterface
+{
+    /**
+     * {@inheritdoc}
+     * - Adds a listener for the `\Symfony\Component\HttpKernel\KernelEvents::EXCEPTION` event.
+     * - Throws previously catched exception.
+     *
+     * @api
+     *
+     * @param \Spryker\Shared\EventDispatcher\EventDispatcherInterface $eventDispatcher
+     * @param \Spryker\Service\Container\ContainerInterface $container
+     *
+     * @return \Spryker\Shared\EventDispatcher\EventDispatcherInterface
+     */
+    public function extend(EventDispatcherInterface $eventDispatcher, ContainerInterface $container): EventDispatcherInterface
+    {
+        $eventDispatcher->addListener(KernelEvents::EXCEPTION, function (GetResponseForExceptionEvent $event) {
+            $this->onKernelException($event);
+        }, -8);
+
+        return $eventDispatcher;
+    }
+
+    /**
+     * @param \Symfony\Component\HttpKernel\Event\GetResponseForExceptionEvent $event
+     *
+     * @throws \Exception
+     *
+     * @return void
+     */
+    public function onKernelException(GetResponseForExceptionEvent $event)
+    {
+        throw $event->getException();
+    }
+}
