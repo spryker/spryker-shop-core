1765255ce58c9db06b6a7bff06085073273e346e
Dima Aseev <dmitriy.aseev@spryker.com>
2019-10-08 19:25:05 +0300

    CC-6552: BC fix for conf bundles and split delivery.

--- Bundles/SalesConfigurableBundleWidget/src/SprykerShop/Yves/SalesConfigurableBundleWidget/Widget/OrderConfiguredBundleWidget.php
+++ Bundles/SalesConfigurableBundleWidget/src/SprykerShop/Yves/SalesConfigurableBundleWidget/Widget/OrderConfiguredBundleWidget.php
@@ -8,6 +8,7 @@
 namespace SprykerShop\Yves\SalesConfigurableBundleWidget\Widget;
 
 use Generated\Shared\Transfer\OrderTransfer;
+use Generated\Shared\Transfer\SalesOrderConfiguredBundleTransfer;
 use Spryker\Yves\Kernel\Widget\AbstractWidget;
 
 /**
@@ -18,14 +19,24 @@ class OrderConfiguredBundleWidget extends AbstractWidget
 {
     protected const PARAMETER_ORDER = 'order';
     protected const PARAMETER_ITEMS = 'items';
+    protected const PARAMETER_SALES_ORDER_CONFIGURED_BUNDLES = 'salesOrderConfiguredBundles';
 
     /**
      * @param \Generated\Shared\Transfer\OrderTransfer $orderTransfer
+     * @param \Generated\Shared\Transfer\ItemTransfer[]|null $itemTransfers
      */
-    public function __construct(OrderTransfer $orderTransfer)
+    public function __construct(OrderTransfer $orderTransfer, ?iterable $itemTransfers = [])
     {
+        if (!count($itemTransfers)) {
+            $itemTransfers = $orderTransfer->getItems();
+        }
+
+        $itemTransfers = $this->mapOrderItems($itemTransfers);
+        $salesOrderConfiguredBundles = $this->mapSalesOrderConfiguredBundles($orderTransfer, $itemTransfers);
+
+        $this->addItemsParameter($itemTransfers);
         $this->addOrderParameter($orderTransfer);
-        $this->addItemsParameter($orderTransfer);
+        $this->addSalesOrderConfiguredBundlesParameter($salesOrderConfiguredBundles);
     }
 
     /**
@@ -55,28 +66,93 @@ class OrderConfiguredBundleWidget extends AbstractWidget
     }
 
     /**
-     * @param \Generated\Shared\Transfer\OrderTransfer $orderTransfer
+     * @param \Generated\Shared\Transfer\ItemTransfer[] $itemTransfers
      *
      * @return void
      */
-    protected function addItemsParameter(OrderTransfer $orderTransfer): void
+    protected function addItemsParameter(iterable $itemTransfers): void
     {
-        $this->addParameter(static::PARAMETER_ITEMS, $this->mapOrderItems($orderTransfer));
+        $this->addParameter(static::PARAMETER_ITEMS, $itemTransfers);
     }
 
     /**
-     * @param \Generated\Shared\Transfer\OrderTransfer $orderTransfer
+     * @param \Generated\Shared\Transfer\SalesOrderConfiguredBundleTransfer[] $salesOrderConfiguredBundles
+     *
+     * @return void
+     */
+    protected function addSalesOrderConfiguredBundlesParameter(iterable $salesOrderConfiguredBundles): void
+    {
+        $this->addParameter(static::PARAMETER_SALES_ORDER_CONFIGURED_BUNDLES, $salesOrderConfiguredBundles);
+    }
+
+    /**
+     * @param \Generated\Shared\Transfer\ItemTransfer[] $itemTransfers
      *
      * @return \Generated\Shared\Transfer\ItemTransfer[]
      */
-    protected function mapOrderItems(OrderTransfer $orderTransfer): array
+    protected function mapOrderItems(iterable $itemTransfers): array
     {
         $items = [];
 
-        foreach ($orderTransfer->getItems() as $itemTransfer) {
+        foreach ($itemTransfers as $itemTransfer) {
             $items[$itemTransfer->getIdSalesOrderItem()] = $itemTransfer;
         }
 
         return $items;
+    }
+
+    /**
+     * @param \Generated\Shared\Transfer\OrderTransfer $orderTransfer
+     * @param \Generated\Shared\Transfer\ItemTransfer[] $itemTransfers
+     *
+     * @return \Generated\Shared\Transfer\SalesOrderConfiguredBundleTransfer[]
+     */
+    protected function mapSalesOrderConfiguredBundles(OrderTransfer $orderTransfer, iterable $itemTransfers): array
+    {
+        $salesOrderConfiguredBundles = [];
+
+        foreach ($orderTransfer->getSalesOrderConfiguredBundles() as $salesOrderConfiguredBundleTransfer) {
+            if ($this->hasSalesOrderItems($salesOrderConfiguredBundleTransfer, $itemTransfers)) {
+                $salesOrderConfiguredBundles[] = $salesOrderConfiguredBundleTransfer;
+            }
+        }
+
+        return $salesOrderConfiguredBundles;
+    }
+
+    /**
+     * @param \Generated\Shared\Transfer\SalesOrderConfiguredBundleTransfer $salesOrderConfiguredBundleTransfer
+     *
+     * @return int[]
+     */
+    protected function getSalesOrderItemIdsFromConfiguredBundle(
+        SalesOrderConfiguredBundleTransfer $salesOrderConfiguredBundleTransfer
+    ): array {
+        $salesOrderItemIds = [];
+
+        foreach ($salesOrderConfiguredBundleTransfer->getSalesOrderConfiguredBundleItems() as $salesOrderConfiguredBundleItemTransfer) {
+            $salesOrderItemIds[] = $salesOrderConfiguredBundleItemTransfer->getIdSalesOrderItem();
+        }
+
+        return $salesOrderItemIds;
+    }
+
+    /**
+     * @param \Generated\Shared\Transfer\SalesOrderConfiguredBundleTransfer $salesOrderConfiguredBundleTransfer
+     * @param \Generated\Shared\Transfer\ItemTransfer[] $itemTransfers
+     *
+     * @return bool
+     */
+    protected function hasSalesOrderItems(
+        SalesOrderConfiguredBundleTransfer $salesOrderConfiguredBundleTransfer,
+        iterable $itemTransfers
+    ): bool {
+        foreach ($itemTransfers as $itemTransfer) {
+            if (in_array($itemTransfer->getIdSalesOrderItem(), $this->getSalesOrderItemIdsFromConfiguredBundle($salesOrderConfiguredBundleTransfer))) {
+                return true;
+            }
+        }
+
+        return false;
     }
 }
