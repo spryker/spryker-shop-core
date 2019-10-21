7cc1ed79bb2dd050b1b946392de03cf5f62bb2e2
Dmitry Lymarenko <dmitry.lymarenko@spryker.com>
2019-10-21 15:59:10 +0300

    PS-11143 Added ConfigurableBundlePage module skeleton, updated navigation data import

---
+++ Bundles/ConfigurableBundlePage/src/SprykerShop/Yves/ConfigurableBundlePage/Controller/ConfiguratorController.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+ * Copyright © 2016-present Spryker Systems GmbH. All rights reserved.
+ * Use of this software requires acceptance of the Evaluation License Agreement. See LICENSE file.
+ */
+
+namespace SprykerShop\Yves\ConfigurableBundlePage\Controller;
+
+use SprykerShop\Yves\ShopApplication\Controller\AbstractController;
+
+class ConfiguratorController extends AbstractController
+{
+    /**
+     * @return void
+     */
+    public function templateSelectionAction(): void
+    {
+        dd(1);
+    }
+}
