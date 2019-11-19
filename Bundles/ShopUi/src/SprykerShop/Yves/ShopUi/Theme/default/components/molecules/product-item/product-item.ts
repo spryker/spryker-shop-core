4705b4c16d239aa0388e2ca1ad25da1b1010817b
danil.moroz <danil.moroz@spryker.com>
2019-11-19 12:56:06 +0200

    CC-7201: Adjust 'product-item' component

--- Bundles/ShopUi/src/SprykerShop/Yves/ShopUi/Theme/default/components/molecules/product-item/product-item.ts
+++ Bundles/ShopUi/src/SprykerShop/Yves/ShopUi/Theme/default/components/molecules/product-item/product-item.ts
@@ -4,8 +4,8 @@ export const EVENT_UPDATE_RATING = 'updateRating';
 
 export interface ProductItemData {
     imageUrl: string;
-    name: string;
-    rating: number;
+    nameValue: string;
+    ratingValue: number;
     defaultPrice: string;
     originalPrice: string;
     detailPageUrl: string;
@@ -15,7 +15,7 @@ export interface ProductItemData {
 export default class ProductItem extends Component {
     protected productImage: HTMLImageElement;
     protected productName: HTMLElement;
-    protected productRating: HTMLElement;
+    protected productRating: HTMLInputElement;
     protected productDefaultPrice: HTMLElement;
     protected productOriginalPrice: HTMLElement;
     protected productLinkDetailPage: HTMLAnchorElement[];
@@ -26,12 +26,29 @@ export default class ProductItem extends Component {
     protected init(): void {
         this.productImage = <HTMLImageElement>this.getElementsByClassName(`${this.jsName}__image`)[0];
         this.productName = <HTMLElement>this.getElementsByClassName(`${this.jsName}__name`)[0];
-        this.productRating = <HTMLElement>this.getElementsByClassName(`${this.jsName}__rating`)[0];
+        this.productRating = <HTMLInputElement>this.getElementsByClassName(`${this.jsName}__rating`)[0];
         this.productDefaultPrice = <HTMLElement>this.getElementsByClassName(`${this.jsName}__default-price`)[0];
         this.productOriginalPrice = <HTMLElement>this.getElementsByClassName(`${this.jsName}__original-price`)[0];
-        this.productLinkDetailPage = <HTMLAnchorElement[]>Array.from(this.getElementsByClassName(`${this.jsName}__link-detail-page`));
-        this.productLinkAddToCart = <HTMLAnchorElement>this.getElementsByClassName(`${this.jsName}__link-add-to-cart`)[0];
+        this.productLinkDetailPage = <HTMLAnchorElement[]>Array.from(this.getElementsByClassName(
+            `${this.jsName}__link-detail-page`
+        ));
+        this.productLinkAddToCart = <HTMLAnchorElement>this.getElementsByClassName(
+            `${this.jsName}__link-add-to-cart`
+        )[0];
+    }
 
+    /**
+     * Sets the product card information.
+     * @param data A data object for setting the product card information.
+     */
+    updateProductItemData(data: ProductItemData): void {
+        this.newImageUrl = data.imageUrl;
+        this.newNameValue = data.nameValue;
+        this.newRatingValue = data.ratingValue;
+        this.newDefaultPrice = data.defaultPrice;
+        this.newOriginalPrice = data.originalPrice;
+        this.newDetailPageUrl = data.detailPageUrl;
+        this.newAddToCartUrl = data.addToCartUrl;
     }
 
     protected set newImageUrl(imageUrl: string) {
@@ -40,14 +57,14 @@ export default class ProductItem extends Component {
         }
     }
 
-    protected set newName(name: string) {
+    protected set newNameValue(name: string) {
         if (this.productName) {
             this.productName.innerText = name;
         }
     }
 
-    protected set newRating(rating: number) {
-        this.dispatchCustomEvent(EVENT_UPDATE_RATING, {rating: rating});
+    protected set newRatingValue(rating: number) {
+        this.dispatchCustomEvent(EVENT_UPDATE_RATING, {rating});
     }
 
     protected set newDefaultPrice(defaultPrice: string) {
@@ -71,6 +88,69 @@ export default class ProductItem extends Component {
     protected set newAddToCartUrl(addToCartUrl: string) {
         if (this.productLinkAddToCart) {
             this.productLinkAddToCart.href = addToCartUrl;
+        }
+    }
+
+    /**
+     * Gets the product card image URL.
+     */
+    get imageUrl(): string {
+        if (this.productImage) {
+            return this.productImage.src;
+        }
+    }
+
+    /**
+     * Gets the product card name.
+     */
+    get nameValue(): string {
+        if (this.productName) {
+            return this.productName.innerText;
+        }
+    }
+
+    /**
+     * Gets the product card rating.
+     */
+    get ratingValue(): number {
+        if (this.productRating) {
+            return Number(this.productRating.value);
+        }
+    }
+
+    /**
+     * Gets the product card default price.
+     */
+    get defaultPrice(): string {
+        if (this.productDefaultPrice) {
+            return this.productDefaultPrice.innerText;
+        }
+    }
+
+    /**
+     * Gets the product card original price.
+     */
+    get originalPrice(): string {
+        if (this.productOriginalPrice) {
+            return this.productOriginalPrice.innerText;
+        }
+    }
+
+    /**
+     * Gets the product card detail page URL.
+     */
+    get detailPageUrl(): string {
+        if (this.productLinkDetailPage) {
+            return this.productLinkDetailPage[0].href;
+        }
+    }
+
+    /**
+     * Gets the product card 'add to cart' URL.
+     */
+    get addToCartUrl(): string {
+        if (this.productLinkAddToCart) {
+            return this.productLinkAddToCart.href;
         }
     }
 }
