bf90c50d66323276087dc3b1394ad39ed583f6ef
Stanislav Matveyev <stanislav.matveyev@spryker.com>
2019-11-27 17:26:31 +0200

    CMS-2467: forced version dependency; moved plugin into correct folder

--- Bundles/CmsSlotBlockWidget/src/SprykerShop/Yves/CmsSlotBlockWidget/Plugin/ShopCmsSlot/CmsSlotBlockWidgetCmsSlotContentPlugin.php
+++ Bundles/CmsSlotBlockWidget/src/SprykerShop/Yves/CmsSlotBlockWidget/Plugin/ShopCmsSlot/CmsSlotBlockWidgetCmsSlotContentPlugin.php
@@ -5,7 +5,7 @@
  * Use of this software requires acceptance of the Evaluation License Agreement. See LICENSE file.
  */
 
-namespace SprykerShop\Yves\CmsSlotBlockWidget\Plugin;
+namespace SprykerShop\Yves\CmsSlotBlockWidget\Plugin\ShopCmsSlot;
 
 use Generated\Shared\Transfer\CmsSlotContentRequestTransfer;
 use Generated\Shared\Transfer\CmsSlotContentResponseTransfer;
