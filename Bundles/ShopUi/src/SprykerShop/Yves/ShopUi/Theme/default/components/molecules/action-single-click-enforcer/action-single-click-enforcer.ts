861d4fcfbef5804fecdb658d44946c270a0e3e96
k.kravchenko <kostiantyn.Kravchenko@spryker.com>
2019-12-26 16:13:01 +0200

    PS-9761: Fixing action-single-click-enforcer component

--- Bundles/ShopUi/src/SprykerShop/Yves/ShopUi/Theme/default/components/molecules/action-single-click-enforcer/action-single-click-enforcer.ts
+++ Bundles/ShopUi/src/SprykerShop/Yves/ShopUi/Theme/default/components/molecules/action-single-click-enforcer/action-single-click-enforcer.ts
@@ -19,25 +19,17 @@ export default class ActionSingleClickEnforcer extends Component {
 
     protected onTargetClick(event: Event): void {
         const targetElement = <HTMLElement>event.currentTarget;
-        const isDisabled: boolean = targetElement.hasAttribute('disabled');
         const isLink: boolean = targetElement.matches('a');
         const form: HTMLFormElement = targetElement.closest('form');
         const submitButton = form ? <HTMLButtonElement | HTMLInputElement>form.querySelector('[type="submit"]')
             || <HTMLButtonElement>form.getElementsByTagName('button')[0] : undefined;
 
-        if (isDisabled) {
-            event.preventDefault();
-
-            return;
+        if (submitButton) {
+            this.disableElement(submitButton);
         }
 
         if (form && submitButton) {
-            form.addEventListener('submit', () => {
-                this.disableElement(targetElement);
-            });
-            submitButton.click();
-
-            return;
+            form.submit();
         }
 
         if (isLink) {
