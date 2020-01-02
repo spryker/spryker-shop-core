48bb0c4fb0fb22c8362a9b6340d1a91628d34c22
k.kravchenko <kostiantyn.Kravchenko@spryker.com>
2020-01-02 11:54:24 +0200

    PS-9761: Refactoring after architect code review.

--- Bundles/ShopUi/src/SprykerShop/Yves/ShopUi/Theme/default/components/molecules/action-single-click-enforcer/action-single-click-enforcer.ts
+++ Bundles/ShopUi/src/SprykerShop/Yves/ShopUi/Theme/default/components/molecules/action-single-click-enforcer/action-single-click-enforcer.ts
@@ -20,6 +20,7 @@ export default class ActionSingleClickEnforcer extends Component {
     protected onTargetClick(event: Event): void {
         const targetElement = <HTMLElement>event.currentTarget;
         const isLink: boolean = targetElement.matches('a');
+        const isButton: boolean = targetElement.matches('button');
 
         if (isLink) {
             const link = <HTMLLinkElement>targetElement;
@@ -33,9 +34,10 @@ export default class ActionSingleClickEnforcer extends Component {
 
         if (form) {
             const buttonType = targetElement.getAttribute('type');
-            const isSubmit = buttonType === 'submit' || buttonType === 'image' || !buttonType;
+            const isSubmit = buttonType === 'submit' || buttonType === 'image';
+            const isButtonSubmit = isButton && !buttonType;
 
-            if (isSubmit) {
+            if (isSubmit || isButtonSubmit) {
                 form.submit();
             }
         }
@@ -48,10 +50,8 @@ export default class ActionSingleClickEnforcer extends Component {
             return;
         }
 
-        const url: string = (<HTMLLinkElement>targetElement).href;
-
         targetElement.dataset.disabled = 'true';
-        window.location.href = url;
+        window.location.href = targetElement.href;
     }
 
     protected disableButton(targetElement: HTMLElement): void {
