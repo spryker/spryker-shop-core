442e1f0fb66063e171a5820ed3b72a5c6c726996
maslov <dev.maslov.a@gmail.com>
2020-04-24 16:42:24 +0300

    PS-13062: Fixed according to AA CR

--- Bundles/ShopUi/src/SprykerShop/Yves/ShopUi/Theme/default/components/molecules/form-value-setter/index.ts
+++
@@ -1,2 +0,0 @@
-import register from 'ShopUi/app/registry';
-export default register('form-value-setter', () => import(/* webpackMode: "eager" */'./form-value-setter'));
---
+++ Bundles/ShopUi/src/SprykerShop/Yves/ShopUi/Theme/default/components/molecules/form-value-submitter/form-value-submitter.ts
@@ -0,0 +1,51 @@
+import Component from 'ShopUi/models/component';
+
+export default class FormValueSubmitter extends Component {
+    protected form: HTMLFormElement;
+    protected triggers: HTMLElement[];
+    protected target: HTMLInputElement;
+
+    protected readyCallback(): void {}
+
+    protected init(): void {
+        this.form = <HTMLFormElement>document.getElementsByClassName(this.formClassName)[0];
+        this.triggers = <HTMLElement[]>Array.from(document.getElementsByClassName(this.triggerClassName));
+        this.target = <HTMLInputElement>document.getElementsByClassName(this.targetClassName)[0];
+        this.mapEvents();
+    }
+
+    protected mapEvents(): void {
+        this.mapTriggerClickEvent();
+    }
+
+    protected mapTriggerClickEvent(): void {
+        this.triggers.forEach((trigger: HTMLElement) => {
+            trigger.addEventListener('click', () => this.setValue(trigger));
+        });
+    }
+
+    protected setValue(trigger: HTMLElement): void {
+        const value: string = trigger.getAttribute(this.valueAttribute);
+        const submitEvent: Event = new Event('submit');
+
+        this.target.value = value;
+        this.form.dispatchEvent(submitEvent);
+        this.form.submit();
+    }
+
+    protected get formClassName(): string {
+        return this.getAttribute('form-class-name');
+    }
+
+    protected get triggerClassName(): string {
+        return this.getAttribute('trigger-class-name');
+    }
+
+    protected get targetClassName(): string {
+        return this.getAttribute('target-class-name');
+    }
+
+    protected get valueAttribute(): string {
+        return this.getAttribute('value-field');
+    }
+}
