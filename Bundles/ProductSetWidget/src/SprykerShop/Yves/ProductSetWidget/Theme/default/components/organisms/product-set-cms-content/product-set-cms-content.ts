7a0bd12ed652b09c0a5f669a53f668b9bf2886f2
danil.moroz <danil.moroz@spryker.com>
2020-05-06 13:10:27 +0300

    CC-8778: Fixed comments after CR

---
+++ Bundles/ProductSetWidget/src/SprykerShop/Yves/ProductSetWidget/Theme/default/components/organisms/product-set-cms-content/product-set-cms-content.ts
@@ -0,0 +1,7 @@
+import ProductSetDetails from '../product-set-details/product-set-details';
+
+export default class ProductSetCmsContent extends ProductSetDetails {
+    protected init(): void {
+        super.init();
+    }
+}
