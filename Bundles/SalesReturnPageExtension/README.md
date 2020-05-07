b4fe9bc59e6b88309c486e917c972103ad131ebe
ZakirovDamir <damir.zakirov.93@gmail.com>
2020-05-07 12:47:48 +0300

    ps-12934 created SalesReturnPageExtension module

---
+++ Bundles/SalesReturnPageExtension/README.md
@@ -0,0 +1,15 @@
+# SalesReturnPageExtension Module
+[![Build Status](https://travis-ci.org/spryker-shop/sales-return-page-extension.svg)](https://travis-ci.org/spryker-shop/sales-return-page-extension)
+[![Minimum PHP Version](https://img.shields.io/badge/php-%3E%3D%207.2-8892BF.svg)](https://php.net/)
+
+The module provides plugin interfaces for extending sales return page module functionality.
+
+## Installation
+
+```
+composer require spryker-shop/sales-return-page-extension
+```
+
+## Documentation
+
+[Spryker Documentation](https://documentation.spryker.com/module_guide/overview.htm)
---
+++ Bundles/SalesReturnPageExtension/src/SprykerShop/Yves/SalesReturnPageExtension/Dependency/Plugin/SalesReturnPageFormExpanderPluginInterface.php
@@ -0,0 +1,53 @@
+<?php
+
+/**
+ * Copyright © 2016-present Spryker Systems GmbH. All rights reserved.
+ * Use of this software requires acceptance of the Evaluation License Agreement. See LICENSE file.
+ */
+
+namespace SprykerShop\Yves\SalesReturnPageExtension\Dependency\Plugin;
+
+use Generated\Shared\Transfer\ReturnCreateRequestTransfer;
+use Symfony\Component\Form\FormBuilderInterface;
+
+interface SalesReturnPageFormExpanderPluginInterface
+{
+    /**
+     * Specification:
+     *  - Extends return create form metadata field with additional input fields.
+     *
+     * @api
+     *
+     * @param \Symfony\Component\Form\FormBuilderInterface $builder
+     * @param array $options
+     *
+     * @return \Symfony\Component\Form\FormBuilderInterface
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options): FormBuilderInterface;
+
+    /**
+     * Specification:
+     * - Expands Return create form data with product bundles.
+     *
+     * @api
+     *
+     * @param array $formData
+     *
+     * @return array
+     */
+    public function expandFormData(array $formData): array;
+
+    /**
+     * Specification:
+     * - Handles Return create form.
+     * - Converts submitted product bundles data to ReturnCreateRequestTransfer.
+     *
+     * @api
+     *
+     * @param array $returnItemsList
+     * @param \Generated\Shared\Transfer\ReturnCreateRequestTransfer $returnCreateRequestTransfer
+     *
+     * @return \Generated\Shared\Transfer\ReturnCreateRequestTransfer
+     */
+    public function handleFormData(array $returnItemsList, ReturnCreateRequestTransfer $returnCreateRequestTransfer): ReturnCreateRequestTransfer;
+}
