0f19ab0a35e3241192a2161c50064a941d0b7ec9
danil.moroz <danil.moroz@spryker.com>
2020-05-28 17:14:26 +0300

    Merge branches 'bugfix/cc-8778-product-set-add-to-cart-correct-abstract' and 'master' of github.com:spryker/suite-nonsplit into bugfix/cc-8778-product-set-add-to-cart-correct-abstract
    # Conflicts:
    #	composer.lock

--- Bundles/ProductSetWidget/src/SprykerShop/Yves/ProductSetWidget/Theme/default/components/organisms/product-set-cms-content/product-set-cms-content.ts
+++ Bundles/ProductSetWidget/src/SprykerShop/Yves/ProductSetWidget/Theme/default/components/organisms/product-set-cms-content/product-set-cms-content.ts
@@ -1,7 +1,3 @@
 import ProductSetDetails from '../product-set-details/product-set-details';
 
-export default class ProductSetCmsContent extends ProductSetDetails {
-    protected init(): void {
-        super.init();
-    }
-}
+export default class ProductSetCmsContent extends ProductSetDetails {}
