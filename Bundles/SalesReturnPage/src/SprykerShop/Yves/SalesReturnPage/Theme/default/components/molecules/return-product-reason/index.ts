3b3a64d726e69b7f7d5961d989129af5234bc4ad
Andrey Maslov <37797323+andrew-maslov@users.noreply.github.com>
2020-07-17 14:54:01 +0300

    Update Bundles/SalesReturnPage/src/SprykerShop/Yves/SalesReturnPage/Theme/default/components/molecules/return-product-reason/index.ts
    Co-authored-by: Nikolay <41688490+zamlely@users.noreply.github.com>

--- Bundles/SalesReturnPage/src/SprykerShop/Yves/SalesReturnPage/Theme/default/components/molecules/return-product-reason/index.ts
+++ Bundles/SalesReturnPage/src/SprykerShop/Yves/SalesReturnPage/Theme/default/components/molecules/return-product-reason/index.ts
@@ -1,4 +1,5 @@
 import register from 'ShopUi/app/registry';
 export default register('return-product-reason', () => import(
-    /* webpackMode: "eager" */
+    /* webpackMode: "lazy" */
+    /* webpackChunkName: "return-product-reason" */
     './return-product-reason'));
