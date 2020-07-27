ca2c28f92f18a5007918e69364696b754389d7a1
maslov <dev.maslov.a@gmail.com>
2020-07-27 13:55:41 +0300

    TE-7013: completed splitting css

---
+++ Bundles/ShopUi/src/SprykerShop/Yves/ShopUi/Theme/default/components/molecules/style-loader/style-loader.ts
@@ -0,0 +1,30 @@
+import Component from 'ShopUi/models/component';
+import {EVENT_UPDATE_CART_QUANTITY} from "ShopUi/components/molecules/cart-counter/cart-counter";
+
+export default class StyleLoader extends Component {
+    protected readyCallback(): void {}
+
+    protected init(): void {
+        this.mapEvents();
+    }
+
+    protected mapEvents(): void {
+        this.mapLoadEvent();
+    }
+
+    protected mapLoadEvent(): void {
+        window.addEventListener('load', () => this.addCss());
+    }
+
+    protected addCss(): void {
+        var link = document.createElement('link');
+        link.href = this.pathToCSS;
+        link.rel = 'stylesheet';
+
+        document.getElementsByTagName('head')[0].appendChild(link);
+    }
+
+    protected get pathToCSS(): string {
+        return this.getAttribute('path-to-css');
+    }
+}
