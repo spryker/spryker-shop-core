43151f51014f50101041696537ea853cb21b6212
denys.sokolov <denys.sokolov@spryker.com>
2020-09-01 17:53:24 +0300

    CC-9231: Added `ProductConfigurationQuoteValidatorWidget` widget.

---
+++ Bundles/ProductConfigurationWidget/src/SprykerShop/Yves/ProductConfigurationWidget/Widget/ProductConfigurationQuoteValidatorWidget.php
@@ -0,0 +1,58 @@
+<?php
+
+/**
+ * Copyright © 2016-present Spryker Systems GmbH. All rights reserved.
+ * Use of this software requires acceptance of the Evaluation License Agreement. See LICENSE file.
+ */
+
+namespace SprykerShop\Yves\ProductConfigurationWidget\Widget;
+
+use Generated\Shared\Transfer\QuoteTransfer;
+use Spryker\Yves\Kernel\Widget\AbstractWidget;
+
+/**
+ * @method \SprykerShop\Yves\ProductConfigurationWidget\ProductConfigurationWidgetFactory getFactory()
+ * @method \SprykerShop\Yves\ProductConfigurationWidget\ProductConfigurationWidgetConfig getConfig()
+ */
+class ProductConfigurationQuoteValidatorWidget extends AbstractWidget
+{
+    protected const PARAMETER_IS_QUOTE_PRODUCT_CONFIGURATION_VALID = 'isQuoteProductConfigurationValid';
+
+    /**
+     * @param \Generated\Shared\Transfer\QuoteTransfer $quoteTransfer
+     */
+    public function __construct(QuoteTransfer $quoteTransfer)
+    {
+        $this->addIsQuoteProductConfigurationValidParameter($quoteTransfer);
+    }
+
+    /**
+     * @return string
+     */
+    public static function getName(): string
+    {
+        return 'ProductConfigurationQuoteValidatorWidget';
+    }
+
+    /**
+     * @return string
+     */
+    public static function getTemplate(): string
+    {
+        return '@ProductConfigurationWidget/views/product-configuration-quote-validator-widget/product-configuration-quote-validator-widget.twig';
+    }
+
+    /**
+     * @param \Generated\Shared\Transfer\QuoteTransfer $quoteTransfer
+     *
+     * @return void
+     */
+    protected function addIsQuoteProductConfigurationValidParameter(QuoteTransfer $quoteTransfer): void
+    {
+        $isQuoteProductConfigurationValid = $this->getFactory()
+            ->getProductConfigurationClient()
+            ->isQuoteProductConfigurationValid($quoteTransfer);
+
+        $this->addParameter(static::PARAMETER_IS_QUOTE_PRODUCT_CONFIGURATION_VALID, $isQuoteProductConfigurationValid);
+    }
+}
