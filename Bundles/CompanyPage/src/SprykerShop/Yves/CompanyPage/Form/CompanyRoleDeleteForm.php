354e0e8275b66ada4bd6e3029331b6feb4ae6c69
René Klatt <rene.klatt@spryker.com>
2020-09-15 05:11:31 +0000

    TE-7001 Replaced usage of new validator with old one.

--- Bundles/CompanyPage/src/SprykerShop/Yves/CompanyPage/Form/CompanyRoleDeleteForm.php
+++ Bundles/CompanyPage/src/SprykerShop/Yves/CompanyPage/Form/CompanyRoleDeleteForm.php
@@ -13,8 +13,8 @@ use Symfony\Component\Form\Extension\Core\Type\HiddenType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\GreaterThanOrEqual;
 use Symfony\Component\Validator\Constraints\NotBlank;
-use Symfony\Component\Validator\Constraints\Positive;
 
 /**
  * @method \SprykerShop\Yves\CompanyPage\CompanyPageConfig getConfig()
@@ -57,7 +57,7 @@ class CompanyRoleDeleteForm extends AbstractType
         $builder->add(static::FIELD_ID_COMPANY_ROLE, HiddenType::class, [
             'constraints' => [
                 new NotBlank(),
-                new Positive(),
+                new GreaterThanOrEqual(['value' => 1]),
             ],
         ]);
 
