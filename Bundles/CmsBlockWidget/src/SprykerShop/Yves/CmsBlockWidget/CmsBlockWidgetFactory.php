3a23a2f325b844d09a56da2bcefd7ca9070245fd
gechetspr <serhii.chepela@spryker.com>
2020-09-16 12:41:48 +0300

    TE-5394 moved twig functions inside of plugins

--- Bundles/CmsBlockWidget/src/SprykerShop/Yves/CmsBlockWidget/CmsBlockWidgetFactory.php
+++ Bundles/CmsBlockWidget/src/SprykerShop/Yves/CmsBlockWidget/CmsBlockWidgetFactory.php
@@ -7,73 +7,16 @@
 
 namespace SprykerShop\Yves\CmsBlockWidget;
 
-use Spryker\Shared\Twig\TwigFunctionProvider;
 use Spryker\Yves\CmsContentWidget\Plugin\CmsTwigContentRendererPluginInterface;
 use Spryker\Yves\Kernel\AbstractFactory;
 use SprykerShop\Yves\CmsBlockWidget\Dependency\Client\CmsBlockWidgetToCmsBlockStorageClientInterface;
 use SprykerShop\Yves\CmsBlockWidget\Dependency\Client\CmsBlockWidgetToStoreClientInterface;
-use SprykerShop\Yves\CmsBlockWidget\Twig\CmsBlockPlaceholderTwigFunctionProvider;
-use SprykerShop\Yves\CmsBlockWidget\Twig\CmsBlockTwigFunctionProvider;
 use SprykerShop\Yves\CmsBlockWidget\Validator\CmsBlockValidator;
 use SprykerShop\Yves\CmsBlockWidget\Validator\CmsBlockValidatorInterface;
 use Symfony\Contracts\Translation\TranslatorInterface;
-use Twig\TwigFunction;
 
 class CmsBlockWidgetFactory extends AbstractFactory
 {
-    /**
-     * @param string $localeName
-     *
-     * @return \Spryker\Shared\Twig\TwigFunctionProvider
-     */
-    public function createCmsBlockTwigFunctionProvider(string $localeName): TwigFunctionProvider
-    {
-        return new CmsBlockTwigFunctionProvider(
-            $this->getCmsBlockStorageClient(),
-            $this->getStoreClient(),
-            $this->createCmsBlockValidator(),
-            $localeName
-        );
-    }
-
-    /**
-     * @param string $localeName
-     *
-     * @return \Twig\TwigFunction
-     */
-    public function createCmsBlockTwigFunction(string $localeName): TwigFunction
-    {
-        $functionProvider = $this->createCmsBlockTwigFunctionProvider($localeName);
-
-        return new TwigFunction(
-            $functionProvider->getFunctionName(),
-            $functionProvider->getFunction(),
-            $functionProvider->getOptions()
-        );
-    }
-
-    /**
-     * @return \Spryker\Shared\Twig\TwigFunctionProvider
-     */
-    public function createCmsBlockPlaceholderTwigFunctionProvider(): TwigFunctionProvider
-    {
-        return new CmsBlockPlaceholderTwigFunctionProvider($this->getTranslatorService(), $this->getCmsTwigContentRendererPlugin());
-    }
-
-    /**
-     * @return \Twig\TwigFunction
-     */
-    public function createCmsBlockPlaceholderTwigFunction(): TwigFunction
-    {
-        $functionProvider = $this->createCmsBlockPlaceholderTwigFunctionProvider();
-
-        return new TwigFunction(
-            $functionProvider->getFunctionName(),
-            $functionProvider->getFunction(),
-            $functionProvider->getOptions()
-        );
-    }
-
     /**
      * @return \SprykerShop\Yves\CmsBlockWidget\Validator\CmsBlockValidatorInterface
      */
