314961817f9773aa2533d36763a40cbf0b61c9f3
galitsky <eugen.galitsky@spryker.com>
2020-11-04 11:40:37 +0200

    CC-11591: configurable price chf

--- Bundles/DateTimeConfiguratorPageExample/src/SprykerShop/Configurator/DateTimeConfiguratorPageExample/Theme/ConfiguratorApplication/src/app/app.module.ts
+++ Bundles/DateTimeConfiguratorPageExample/src/SprykerShop/Configurator/DateTimeConfiguratorPageExample/Theme/ConfiguratorApplication/src/app/app.module.ts
@@ -13,6 +13,7 @@ import { FooterComponent } from './footer/footer.component';
 import { ProductDetailsComponent } from './product-details/product-details.component';
 import { JsonOutputComponent } from './json-output/json-output.component';
 import { ConfiguratorService } from '../services/configurator.service';
+import { ConfigurePricePipe } from './product-details/configurator-price.pipe';
 
 export function HttpLoaderFactory(httpClient: HttpClient): TranslateLoader {
     return new TranslateHttpLoader(httpClient, './dist/assets/i18n/');
@@ -26,6 +27,7 @@ export function HttpLoaderFactory(httpClient: HttpClient): TranslateLoader {
         FooterComponent,
         ProductDetailsComponent,
         JsonOutputComponent,
+        ConfigurePricePipe,
     ],
     imports: [
         BrowserModule,
