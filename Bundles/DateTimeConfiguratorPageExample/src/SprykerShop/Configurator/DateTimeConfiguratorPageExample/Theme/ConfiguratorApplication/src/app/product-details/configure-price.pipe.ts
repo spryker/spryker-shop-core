63ebe1d0567213cbe145a7f0db88a41751ff8e7b
Yuriy Gerton <yuriy.gerton@spryker.com>
2020-11-13 15:40:35 +0200

    CC-11764: fix total price calculation

--- Bundles/DateTimeConfiguratorPageExample/src/SprykerShop/Configurator/DateTimeConfiguratorPageExample/Theme/ConfiguratorApplication/src/app/product-details/configure-price.pipe.ts
+++ Bundles/DateTimeConfiguratorPageExample/src/SprykerShop/Configurator/DateTimeConfiguratorPageExample/Theme/ConfiguratorApplication/src/app/product-details/configure-price.pipe.ts
@@ -6,14 +6,16 @@ import { ConfiguredProduct, VolumePrices } from '../../services/types';
 })
 export class ConfigurePricePipe implements PipeTransform {
     transform(productData: ConfiguredProduct, isTotal = false): number {
+        const quantity = isTotal ? productData.quantity : 1;
+
         if (productData.quantity < 5 || !productData.volume_prices) {
-            return productData.price / 100;
+            return productData.price / 100 * quantity;
         }
 
         const isNetMode = productData.price_mode.toLowerCase().trim() === 'net_mode';
         const volumePrice = (productData.volume_prices as VolumePrices).volume_prices[0];
         const configuredPrice = isNetMode ? volumePrice.net_price : volumePrice.gross_price;
 
-        return (configuredPrice / 100) * (isTotal ? productData.quantity : 1);
+        return configuredPrice / 100 * quantity;
     }
 }
