3da2288bae905e583a8d13af8b8c6298af3726ee
dmytro-sokolovskyi <79465867+dmytro-sokolovskyi@users.noreply.github.com>
2021-04-01 19:38:52 +0300

    CC-9271 Support reordering of configurable products (#1917)
    CC-9271 Support reordering of configurable products

---
+++ Bundles/CustomerReorderWidgetExtension/src/SprykerShop/Yves/CustomerReorderWidgetExtension/Dependency/Plugin/ReorderItemExpanderPluginInterface.php
@@ -0,0 +1,29 @@
+<?php
+
+/**
+ * Copyright © 2016-present Spryker Systems GmbH. All rights reserved.
+ * Use of this software requires acceptance of the Evaluation License Agreement. See LICENSE file.
+ */
+
+namespace SprykerShop\Yves\CustomerReorderWidgetExtension\Dependency\Plugin;
+
+use Generated\Shared\Transfer\OrderTransfer;
+
+/**
+ * Use this plugin to extend items before they added to the cart on the reorder action.
+ */
+interface ReorderItemExpanderPluginInterface
+{
+    /**
+     * Specification:
+     * - Expands items with data from the order items.
+     *
+     * @api
+     *
+     * @param \Generated\Shared\Transfer\ItemTransfer[] $itemTransfers
+     * @param \Generated\Shared\Transfer\OrderTransfer $orderTransfer
+     *
+     * @return \Generated\Shared\Transfer\ItemTransfer[]
+     */
+    public function expand(array $itemTransfers, OrderTransfer $orderTransfer): array;
+}
---
+++ Bundles/SalesProductConfigurationWidget/src/SprykerShop/Yves/SalesProductConfigurationWidget/Dependency/Client/SalesProductConfigurationWidgetToSalesProductConfigurationClientInterface.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+ * Copyright © 2016-present Spryker Systems GmbH. All rights reserved.
+ * Use of this software requires acceptance of the Evaluation License Agreement. See LICENSE file.
+ */
+
+namespace SprykerShop\Yves\SalesProductConfigurationWidget\Dependency\Client;
+
+use Generated\Shared\Transfer\OrderTransfer;
+
+interface SalesProductConfigurationWidgetToSalesProductConfigurationClientInterface
+{
+    /**
+     * @param \Generated\Shared\Transfer\ItemTransfer[] $itemTransfers
+     * @param \Generated\Shared\Transfer\OrderTransfer $orderTransfer
+     *
+     * @return \Generated\Shared\Transfer\ItemTransfer[]
+     */
+    public function expandItemsWithProductConfiguration(array $itemTransfers, OrderTransfer $orderTransfer): array;
+}
