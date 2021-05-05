a7c5a9a7fd5431495978a53de186509fd8282bf1
René Klatt <rene.klatt@spryker.com>
2021-05-05 12:01:37 +0200

    TE-9022 Add possibility to use eventual consistency (#1936)
    TE-9022 Eventual consistency for Cart

---
+++ Bundles/CartPage/src/SprykerShop/Yves/CartPage/CartPageConfig.php
@@ -0,0 +1,23 @@
+<?php
+
+/**
+ * Copyright © 2016-present Spryker Systems GmbH. All rights reserved.
+ * Use of this software requires acceptance of the Evaluation License Agreement. See LICENSE file.
+ */
+
+namespace SprykerShop\Yves\CartPage;
+
+use Spryker\Yves\Kernel\AbstractBundleConfig;
+
+class CartPageConfig extends AbstractBundleConfig
+{
+    /**
+     * @api
+     *
+     * @return bool
+     */
+    public function isQuoteValidationEnabled(): bool
+    {
+        return true;
+    }
+}
