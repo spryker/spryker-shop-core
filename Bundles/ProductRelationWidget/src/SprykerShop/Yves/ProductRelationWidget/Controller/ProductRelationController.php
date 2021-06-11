b5ebcc54357fcd0db1fbc898c5bd0bb1778f7f81
Svyatoslav Varpikhovsky <varpihovsky@gmail.com>
2021-06-11 16:03:43 +0300

    TE-9020 FE debuging and refactoring

--- Bundles/ProductRelationWidget/src/SprykerShop/Yves/ProductRelationWidget/Controller/ProductRelationController.php
+++ Bundles/ProductRelationWidget/src/SprykerShop/Yves/ProductRelationWidget/Controller/ProductRelationController.php
@@ -9,18 +9,13 @@ namespace SprykerShop\Yves\ProductRelationWidget\Controller;
 
 use Spryker\Yves\Kernel\View\View;
 use SprykerShop\Yves\ShopApplication\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\Response;
 
 /**
  * @method \SprykerShop\Yves\ProductRelationWidget\ProductRelationWidgetFactory getFactory()
  */
 class ProductRelationController extends AbstractController
 {
-    protected const KEY_CODE = 'code';
-    protected const KEY_HTML = 'html';
-
     /**
      * @param \Symfony\Component\HttpFoundation\Request $request
      *
@@ -28,49 +23,18 @@ class ProductRelationController extends AbstractController
      */
     public function getUpsellingProductsWidgetAjaxAction(Request $request): View
     {
-        return $this->test();
+        return $this->executeGetUpsellingProductsWidgetAjaxAction();
     }
 
     /**
-     * @param \Symfony\Component\HttpFoundation\Request $request
-     *
-     * @return \Symfony\Component\HttpFoundation\JsonResponse
+     * @return \Spryker\Yves\Kernel\View\View
      */
-    public function getUpsellingProductsWidgetAjaxAction2(Request $request): JsonResponse
-    {
-        $response = $this->executeGetUpsellingProductsWidgetAjaxAction();
-
-        return $this->jsonResponse(
-            $response
-        );
-    }
-
-    protected function test(): View
-    {
-            $viewData = [
-                'cart' => $this->getFactory()->getCartClient()->getQuote(),
-            ];
-
-            return $this->view($viewData, [], '@ProductRelationWidget/views/ajax-upselling-widget/ajax-upselling-widget.twig');
-    }
-
-    /**
-     * @return string[]
-     */
-    protected function executeGetUpsellingProductsWidgetAjaxAction(): array
+    protected function executeGetUpsellingProductsWidgetAjaxAction(): View
     {
         $viewData = [
-           'cart' => $this->getFactory()->getCartClient()->getQuote(),
+            'cart' => $this->getFactory()->getCartClient()->getQuote(),
         ];
 
-        $upsellingWidgetHtml = $this->renderView(
-            '@ProductRelationWidget/views/ajax-upselling-widget/ajax-upselling-widget.twig',
-            $viewData
-        )->getContent();
-
-        return [
-            static::KEY_CODE => Response::HTTP_OK,
-            static::KEY_HTML => $upsellingWidgetHtml,
-        ];
+        return $this->view($viewData, [], '@ProductRelationWidget/views/ajax-upselling-widget/ajax-upselling-widget.twig');
     }
 }
