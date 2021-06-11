a724fa8704e1e725fb2f8df3db83913554cc8746
Svyatoslav Varpikhovsky <varpihovsky@gmail.com>
2021-06-11 16:03:53 +0300

    TE-9020 FE debuging and refactoring

--- Bundles/ShopUi/src/SprykerShop/Yves/ShopUi/Theme/default/app/index.ts
+++ Bundles/ShopUi/src/SprykerShop/Yves/ShopUi/Theme/default/app/index.ts
@@ -44,14 +44,13 @@ function isComponent(element: Element): boolean {
 }
 
 async function mountComponents(): Promise<void> {
-    console.log('mountComponents >??>>>')
     const promises: Array<Promise<Element[]>> = getCandidates().map((candidate: Candidate) => candidate.define());
     const elements: Element[][] = await Promise.all(promises);
 
     elements.forEach((elementSet: Element[]) =>
         elementSet
             .filter((element: Element) => isComponent(element))
-            // .filter((component: Component) => !component.isMounted)
+            .filter((component: Component) => !component.isMounted)  // if disable filter starting work
             .forEach((component: Component) => mountComponent(component)),
     );
 }
@@ -74,7 +73,6 @@ async function mountComponents(): Promise<void> {
  * @returns Void promise as the mounting process is asyncronous.
  */
 export async function mount(): Promise<void> {
-    console.log('MOUNT2  >>>');
     try {
         await mountComponents();
         dispatchCustomEvent(config().events.mount);
@@ -90,7 +88,6 @@ export async function mount(): Promise<void> {
             isBootstrap = false;
         }
     } catch (err) {
-        console.log(config().events.error)
         dispatchCustomEvent(config().events.error, err);
     }
 }
