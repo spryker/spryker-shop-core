6f2c250e25205897d97b582b0e0ca2dcc790b2f5
maslov <dev.maslov.a@gmail.com>
2021-07-12 15:49:19 +0300

    CC-10184: Fix submitting form

--- Bundles/MultiCartWidget/src/SprykerShop/Yves/MultiCartWidget/Theme/default/components/molecules/mini-cart-radio/mini-cart-radio.ts
+++ Bundles/MultiCartWidget/src/SprykerShop/Yves/MultiCartWidget/Theme/default/components/molecules/mini-cart-radio/mini-cart-radio.ts
@@ -3,21 +3,33 @@ import Component from 'ShopUi/models/component';
 export default class MiniCartRadio extends Component {
     /**
      * The radio button element.
+     *
+     * @deprecated
      */
     radio: HTMLElement;
+    protected form: HTMLFormElement;
 
-    protected readyCallback(): void {
+    protected readyCallback(): void {}
+
+    protected init(): void {
         this.radio = <HTMLElement>this.getElementsByClassName(`${this.jsName}__input`)[0];
+        this.form = <HTMLFormElement>this.getElementsByClassName(`${this.jsName}__form`)[0];
 
         this.mapEvents();
     }
 
     private mapEvents(): void {
-        this.onclick = () => (window.location.href = this.locationUrl);
+        this.addEventListener('click', () => this.onMiniCartRadioClick())
+    }
+
+    protected onMiniCartRadioClick(): void {
+        this.form.submit();
     }
 
     /**
      * Gets the location url from a checked radio button.
+     *
+     * @deprecated
      */
     get locationUrl(): string {
         return this.radio.dataset.href;
