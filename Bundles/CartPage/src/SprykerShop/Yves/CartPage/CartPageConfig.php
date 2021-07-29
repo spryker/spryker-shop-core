c8e94e630cd77bbdda96bd94af0051bd90a006af
Svytoslav V <svytoslav.varpikhovsky@spryker.com>
2021-07-29 10:42:13 +0300

    TE-9021 Disable ajax loading for cart items

--- Bundles/CartPage/src/SprykerShop/Yves/CartPage/CartPageConfig.php
+++ Bundles/CartPage/src/SprykerShop/Yves/CartPage/CartPageConfig.php
@@ -21,7 +21,7 @@ class CartPageConfig extends AbstractBundleConfig
      */
     public function isCartCartItemsViaAjaxLoadEnabled(): bool
     {
-        return true;
+        return false;
     }
 
     /**
