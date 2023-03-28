f460bcc04469be51f22e0b0d1d746751fd9eeed2
Artem Siminenko <artem.siminenko@gmail.com>
2023-03-28 14:28:19 +0200

    CC-16903 Adjusted login mechanism for Backoffice and Merchant Portal. (#2332)
    CC-16903 Adjusted login mechanism for Backoffice and Merchant Portal.

--- Bundles/SecurityBlockerPage/src/SprykerShop/Yves/SecurityBlockerPage/SecurityBlockerPageConfig.php
+++ Bundles/SecurityBlockerPage/src/SprykerShop/Yves/SecurityBlockerPage/SecurityBlockerPageConfig.php
@@ -12,14 +12,14 @@ use Spryker\Yves\Kernel\AbstractBundleConfig;
 class SecurityBlockerPageConfig extends AbstractBundleConfig
 {
     /**
-     * @uses \Spryker\Glue\SecurityBlockerRestApi\SecurityBlockerRestApiConfig::SECURITY_BLOCKER_CUSTOMER_ENTITY_TYPE
+     * @uses \Spryker\Client\SecurityBlockerStorefrontCustomer\SecurityBlockerStorefrontCustomerConfig::SECURITY_BLOCKER_CUSTOMER_ENTITY_TYPE
      *
      * @var string
      */
     public const SECURITY_BLOCKER_CUSTOMER_ENTITY_TYPE = 'customer';
 
     /**
-     * @uses \Spryker\Glue\SecurityBlockerRestApi\SecurityBlockerRestApiConfig::SECURITY_BLOCKER_AGENT_ENTITY_TYPE
+     * @uses \Spryker\Client\SecurityBlockerStorefrontAgent\SecurityBlockerStorefrontAgentConfig::SECURITY_BLOCKER_AGENT_ENTITY_TYPE
      *
      * @var string
      */
